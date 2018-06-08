70c0a344b5e572332687bc9dbd87d8d41602e198
Danny Meyer <Danny.Meyer@gmail.com>
2018-06-08 16:25:27 -0400

    working wow

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 fn main() {
-    let test_case1 = vec![0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5];
-
-    let test_case2 = vec![0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0];
+    let test_case1 = vec![0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0, 0.5, 1.0, 0.5, 0.0, -0.5, -1.0];
+    
     let yd = yin_difference(test_case1);
-    println!("yin_difference: {:?}", yd);
+    println!("yin_difference: \n {:?}", yd);
     let cmnd = yin_cumulative_mean_normalized_difference(yd);
-    println!("cum_mean_norm_difference {:?}", cmnd);
+    println!("cum_mean_norm_difference \n {:?}", cmnd);
+    
+    let yat = yin_absolute_threshold(cmnd.clone());
+    let tau: usize;
+    let probability: f32;
+    match yat {
+        Some(yat) => {
+            println!("absolute_threshold \n {:?}", yat);
+            probability = 1.0 - cmnd[yat];
+            tau = yat;
+            println!("probability \n {}", probability);
+            },
+        _ => {
+            println!("{}", "Did not work"); 
+            panic!();
+        }
+    } 
+    let better_tau = yin_parabolic_interpolation(cmnd, tau);
+    println!("better tau after parabolic interpolation \n {:?}", better_tau);
 
+    let pitch_in_hertz = 4410.0 / better_tau;
+    println!("pitch in hertz {:?}", pitch_in_hertz)
 }
 
 fn yin_difference(buffer: Vec<f32>) -> Vec<f32> {
@@ -39,12 +58,12 @@ fn yin_cumulative_mean_normalized_difference(buffer: Vec<f32>) -> Vec<f32> {
 
 
 fn yin_absolute_threshold(buffer: Vec<f32>) -> Option<usize> {
-    let threshold = 0.15;
+    let threshold = 0.20;
     let mut iter = buffer
         .iter()
         .enumerate()
         .skip(2)
-        .skip_while(|(_, &sample)| sample < threshold);
+        .skip_while(|(_, &sample)| sample > threshold);
     let tripped_threshold = iter.next()?;
 
     let (_, mut previous_sample) = tripped_threshold;
@@ -58,39 +77,50 @@ fn yin_absolute_threshold(buffer: Vec<f32>) -> Option<usize> {
     Some(buffer.len() - 1)
 }
 
+fn yin_parabolic_interpolation(buffer: Vec<f32>, tau_estimate: usize) -> f32 {
+    let better_tau: f32;
+    let x0: usize;
+    let x2: usize;
 
-// int16_t Yin_absoluteThreshold(Yin *yin){
-// 	int16_t tau;
+	if tau_estimate < 1 {
+        x0 = tau_estimate;
+    } else {
+        x0 = tau_estimate - 1;
+    }
 
-// 	/* Search through the array of cumulative mean values, and look for ones that are over the threshold 
-// 	 * The first two positions in yinBuffer are always so start at the third (index 2) */
-// 	for (tau = 2; tau < yin->halfBufferSize ; tau++) {
-// 		if (yin->yinBuffer[tau] < yin->threshold) {
-// 			while (tau + 1 < yin->halfBufferSize && yin->yinBuffer[tau + 1] < yin->yinBuffer[tau]) {
-// 				tau++;
-// 			}
-// 			/* found tau, exit loop and return
-// 			 * store the probability
-// 			 * From the YIN paper: The yin->threshold determines the list of
-// 			 * candidates admitted to the set, and can be interpreted as the
-// 			 * proportion of aperiodic power tolerated
-// 			 * within a periodic signal.
-// 			 *
-// 			 * Since we want the periodicity and and not aperiodicity:
-// 			 * periodicity = 1 - aperiodicity */
-// 			yin->probability = 1 - yin->yinBuffer[tau];
-// 			break;
-// 		}
-// 	}
+    if tau_estimate + 1 < buffer.len() / 2 {
+		x2 = tau_estimate + 1;
+	} 
+	else {
+		x2 = tau_estimate;
+	}
 
-// 	/* if no pitch found, tau => -1 */
-// 	if (tau == yin->halfBufferSize || yin->yinBuffer[tau] >= yin->threshold) {
-// 		tau = -1;
-// 		yin->probability = 0;
-// 	}
+    if x0 == tau_estimate {
+		if buffer[tau_estimate] <= buffer[x2] {
+			better_tau = tau_estimate as f32;
+		} 
+		else {
+			better_tau = x2 as f32;
+		}
+	} 
 
-// 	return tau;
-// }
+    else if x2 == tau_estimate {
+		if buffer[tau_estimate] <= buffer[x0] {
+			better_tau = tau_estimate as f32;
+		} 
+		else {
+			better_tau = x0 as f32;
+		}
+	} else {
+		let s0: f32 = buffer[x0];
+		let s1: f32 = buffer[tau_estimate];
+		let s2: f32 = buffer[x2];
+
+		better_tau = tau_estimate as f32 + (s2 - s0) / (2.0 * (2.0 * s1 - s2 - s0));
+	}
+
+    better_tau
+}
 
 #[cfg(test)]
 mod tests {
