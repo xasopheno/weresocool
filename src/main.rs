e2c18a961f200d7945be9e1ee4188ac69be188eb
Danny Meyer <Danny.Meyer@gmail.com>
2018-06-13 15:03:47 -0400

    Ready to connect output

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-extern crate sound;
 extern crate portaudio;
-use std::sync::mpsc::channel;
-
+extern crate sound;
+use sound::portaudio_setup::{setup_portaudio_input, setup_portaudio_output};
 use sound::ring_buffer::RingBuffer;
+use sound::settings::{get_default_app_settings, Settings};
+use sound::sine;
 use sound::yin::YinBuffer;
-use sound::{ sine };
-use sound::portaudio_setup::{ setup_portaudio_input,
-//                              setup_portaudio_output
-};
-use sound::settings::{ Settings, get_default_app_settings };
+use std::sync::mpsc::channel;
 
 use portaudio as pa;
 
@@ -27,30 +24,29 @@ fn run() -> Result<(), pa::Error> {
     let pa = pa::PortAudio::new()?;
     let (mut input_stream, input_callback_rx) = setup_portaudio_input(&pa, settings)?;
 
-    let mut input_buffer: RingBuffer<f32> = RingBuffer::<f32>::new(settings.yin_buffer_size as usize);
+    let mut input_buffer: RingBuffer<f32> =
+        RingBuffer::<f32>::new(settings.yin_buffer_size as usize);
 
     input_stream.start()?;
     let mut frequency: f32 = 0.0;
 
     while let true = input_stream.is_active()? {
         match input_callback_rx.recv() {
-                Ok(vec) => {
-                    input_buffer.append(vec);
-                    let mut buffer_vec: Vec<f32> = input_buffer.to_vec();
-                    if buffer_vec.gain() > settings.gain_threshold {
-                        frequency =
-                            buffer_vec
-                            .yin_pitch_detection(settings.sample_rate, settings.threshold)
-                            .floor();
-                    }
+            Ok(vec) => {
+                input_buffer.append(vec);
+                let mut buffer_vec: Vec<f32> = input_buffer.to_vec();
+                if buffer_vec.gain() > settings.gain_threshold {
+                    frequency = buffer_vec
+                        .yin_pitch_detection(settings.sample_rate, settings.threshold)
+                        .floor();
                 }
-                _ => panic!(),
+            }
+            _ => panic!(),
         }
 
         if frequency > 0.0 && frequency < 2000.0 {
             println!("{:?}", frequency);
         }
-
     }
 
     input_stream.stop()?;
--- src/portaudio_setup.rs
+++ src/portaudio_setup.rs
@@ -1,10 +1,13 @@
-use std;
 use portaudio as pa;
-use settings::{ Settings };
+use settings::Settings;
+use std;
 
 use std::sync::mpsc::channel;
 
-pub fn setup_portaudio_input(ref pa: &pa::PortAudio, ref settings: &Settings) -> Result<
+pub fn setup_portaudio_input(
+    ref pa: &pa::PortAudio,
+    ref settings: &Settings,
+) -> Result<
     (
         pa::Stream<pa::NonBlocking, pa::Input<f32>>,
         std::sync::mpsc::Receiver<Vec<f32>>,
@@ -22,55 +25,69 @@ pub fn setup_portaudio_input(ref pa: &pa::PortAudio, ref settings: &Settings) ->
     Ok((input_stream, input_callback_rx))
 }
 
-fn  get_input_settings(ref pa: &pa::PortAudio, ref settings: &Settings) -> Result
-<pa::stream::InputSettings<f32>, pa::Error>
-{
+fn get_input_settings(
+    ref pa: &pa::PortAudio,
+    ref settings: &Settings,
+) -> Result<pa::stream::InputSettings<f32>, pa::Error> {
     let def_input = pa.default_input_device()?;
     let input_info = pa.device_info(def_input)?;
     println!("Default input device info: {:#?}", &input_info);
 
     let latency = input_info.default_low_input_latency;
-    let input_params = pa::StreamParameters::<f32>::new(def_input, settings.channels, settings.interleaved, latency);
+    let input_params = pa::StreamParameters::<f32>::new(
+        def_input,
+        settings.channels,
+        settings.interleaved,
+        latency,
+    );
 
-    let input_settings =
-        pa::InputStreamSettings::new(input_params, settings.sample_rate as f64, settings.input_buffer_size as u32);
+    let input_settings = pa::InputStreamSettings::new(
+        input_params,
+        settings.sample_rate as f64,
+        settings.input_buffer_size as u32,
+    );
 
     Ok(input_settings)
 }
-//
-//
-//pub fn setup_portaudio_output(ref pa: &pa::PortAudio) -> Result<
-//    (
-//        portaudio::Stream<portaudio::NonBlocking, portaudio::Output<f32>>,
-//        std::sync::mpsc::Receiver<Vec<f32>>,
-//    ),
-//    pa::Error,
-//> {
-//
-//    let (output_callback_tx, output_callback_rx) = channel();
-//    let output_settings = get_output_settings(&pa)?;
-//
-//    let output_stream = pa.open_non_blocking_stream(output_settings, move |args| {
-//        output_callback_tx.send(args.buffer.to_vec()).unwrap();
-//        pa::Continue
-//    })?;
-//
-//    Ok((output_stream, output_callback_rx))
-//}
-//
-//
-//fn  get_output_settings(ref pa: &pa::PortAudio) -> Result
-//<pa::stream::OutputSettings<f32>, pa::Error>
-//{
-//    let def_output = pa.default_output_device()?;
-//    let output_info = pa.device_info(def_output)?;
-//    println!("Default output device info: {:#?}", &output_info);
-//
-//    let latency = output_info.default_low_output_latency;
-//    let output_params = pa::StreamParameters::new(def_output, CHANNELS, INTERLEAVED, latency);
-//
-//    let output_settings =
-//        pa::OutputStreamSettings::new(output_params, SAMPLE_RATE as f64, OUTPUT_BUFFER_SIZE as u32);
-//
-//    Ok(output_settings)
-//}
+
+pub fn setup_portaudio_output(
+    ref pa: &pa::PortAudio,
+    ref settings: &Settings,
+) -> Result<
+    (
+        pa::Stream<pa::NonBlocking, pa::Output<f32>>,
+        std::sync::mpsc::Receiver<Vec<f32>>,
+    ),
+    pa::Error,
+> {
+    let (output_callback_tx, output_callback_rx) = channel();
+    let output_settings = get_output_settings(&pa, &settings)?;
+
+    let output_stream = pa.open_non_blocking_stream(output_settings, move |args| {
+        output_callback_tx.send(args.buffer.to_vec()).unwrap();
+        pa::Continue
+    })?;
+
+    Ok((output_stream, output_callback_rx))
+}
+
+fn get_output_settings(
+    ref pa: &pa::PortAudio,
+    ref settings: &Settings,
+) -> Result<pa::stream::OutputSettings<f32>, pa::Error> {
+    let def_output = pa.default_output_device()?;
+    let output_info = pa.device_info(def_output)?;
+    println!("Default output device info: {:#?}", &output_info);
+
+    let latency = output_info.default_low_output_latency;
+    let output_params =
+        pa::StreamParameters::new(def_output, settings.channels, settings.interleaved, latency);
+
+    let output_settings = pa::OutputStreamSettings::new(
+        output_params,
+        settings.sample_rate as f64,
+        settings.output_buffer_size as u32,
+    );
+
+    Ok(output_settings)
+}
