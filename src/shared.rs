a58cf095821a8d9260ce3dd88b1910dc8fadd9e7
Danny Meyer <Danny.Meyer@gmail.com>
2018-06-29 17:06:26 -0400

    some shared

--- src/shared.rs
+++ src/shared.rs
@@ -1,12 +1,12 @@
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 struct Shared {
-    frequency: f32,
-    probability: f32,
-    gain: f32,
+    frequency: AtomicUsize,
+    probability: AtomicUsize,
+    gain: AtomicUsize,
 }
 
-struct SharedUpdater {
+struct SharedAPI {
     frequency: Option<f32>,
     probability: Option<f32>,
     gain: Option<f32>,
@@ -15,24 +15,32 @@ struct SharedUpdater {
 impl Shared {
     pub fn new() -> Shared {
         Shared {
-            frequency: 0.0,
-            probability: 0.0,
-            gain: 0.0,
+            frequency: AtomicUsize::new(0),
+            probability: AtomicUsize::new(0),
+            gain: AtomicUsize::new(0),
         }
     }
 
-    pub fn update(&mut self, updater: SharedUpdater) {
+    pub fn update(&mut self, updater: SharedAPI) {
         match updater.frequency {
           None => None,
-          Some(frequency) => self.frequency = frequency,
+          Some(frequency) => self.frequency = frequency.to_bits(),
         };
         match updater.probability {
             None => None,
-            Some(probability) => self.probability = probability,
+            Some(probability) => self.probability = probability.to_bits(),
         };
         match updater.gain {
             None => None,
-            Some(gain) => self.gain = gain,
+            Some(gain) => self.gain = gain.to_bits(),
         };
+    }
+
+    pub fn get_current_values(&mut self) -> SharedAPI {
+        SharedAPI {
+            frequency: frequency.from_bits(),
+            probability: frequency.from_bits(),
+            gain: frequency.from_bits(),
+        }
     }
 }
