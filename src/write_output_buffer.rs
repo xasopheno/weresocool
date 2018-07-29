e9aa5139dae23c55e063d5beddd83d43ee449b42
Danny <danny.meyer@gmail.com>
2018-07-29 18:54:58 -0400

    wip

--- src/write_output_buffer.rs
+++ src/write_output_buffer.rs
@@ -5,10 +5,10 @@ pub fn write_output_buffer(out_buffer: &mut [f32], stereo_waveform: StereoWavefo
     let mut r_idx = 0;
     for n in 0..out_buffer.len() {
         if n % 2 == 0 {
-            out_buffer[n] = stereo_waveform.l_buffer[l_idx] / 2.0;
+            out_buffer[n] = stereo_waveform.l_buffer[l_idx];
             l_idx += 1
         } else {
-            out_buffer[n] = stereo_waveform.r_buffer[r_idx] / 2.0;
+            out_buffer[n] = stereo_waveform.r_buffer[r_idx];
             r_idx += 1
         }
     }
