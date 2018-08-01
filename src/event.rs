026dbaf8fa84c70b1fd6dd2fb7339e4cf86aa7fa
Danny Meyer <Danny.Meyer@gmail.com>
2018-08-01 12:03:17 -0400

    commas in r_macro

--- src/event.rs
+++ src/event.rs
@@ -130,20 +130,19 @@ impl Mutate<Phrase> for Phrase {
 #[cfg(test)]
 pub mod tests {
     use super::*;
-    use ratios::simple_ratios;
     use settings::get_test_settings;
     use ratios::Pan;
 
     fn test_ratios() -> Vec<R> {
         r![
-            (1, 1, 0.0, 0.5, 1.0)
+            (1, 1, 0.0, 0.5, 1.0),
             (1, 1, 1.0, 0.5, -1.0)
         ]
     }
 
     fn test_ratios_change() -> Vec<R> {
         r![
-            (3, 2, 0.0, 0.6, -1.0)
+            (3, 2, 0.0, 0.6, -1.0),
             (5, 4, 1.5, 0.5, 0.5)
         ]
     }
@@ -172,17 +171,14 @@ pub mod tests {
     }
 
     #[test]
-    fn test_event_render() {
-        let mut oscillator1 = Oscillator::init(test_ratios(), &get_test_settings());
-        let result = Event::new(100.0, test_ratios(), 0.001, 1.0)
+    fn test_render_event() {
+        let mut result = Event::new(100.0, test_ratios(), 0.001, 1.0)
             .mut_ratios(test_ratios_change())
             .transpose(3.0 / 2.0, 0.0)
             .mut_length(2.0, 0.0)
-            .mut_gain(0.9, 0.0)
-            .render(&mut oscillator1);
+            .mut_gain(0.9, 0.0);
 
-        let mut oscillator2 = Oscillator::init(test_ratios(), &get_test_settings());
-        let expected = Event {
+        let mut expected = Event {
             frequency: 150.0,
             ratios: vec![
                 R::atio(3, 2, 0.0, 0.6, Pan::Left),
@@ -192,62 +188,65 @@ pub mod tests {
             ],
             length: 0.002,
             gain: 0.9,
-        }.render(&mut oscillator2);
+        };
+        let mut oscillator1 = Oscillator::init(test_ratios(), &get_test_settings());
+        let mut oscillator2 = Oscillator::init(test_ratios(), &get_test_settings());
 
         assert_eq!(expected, result);
+        assert_eq!(expected.render(&mut oscillator1),
+                   result.render(&mut oscillator2));
     }
 
 
-//    #[test]
-//    fn test_mutate_phrase() {
-//        let mut phrase = Phrase {
-//            events: vec![
-//                Event::new(100.0, test_ratios(), 1.0, 1.0),
-//                Event::new(50.0, test_ratios(), 2.0, 1.0),
-//            ],
-//        };
-//
-//        let result = phrase
-//            .mut_ratios(test_ratios_change())
-//            .transpose(3.0 / 2.0, 0.0)
-//            .mut_length(2.0, 1.0)
-//            .mut_gain(0.9, 0.0);
-//
-//        let expected = Phrase {
-//            events: vec![
-//                Event::new(150.0, test_ratios_change(), 3.0, 0.9),
-//                Event::new(75.0, test_ratios_change(), 5.0, 0.9),
-//            ],
-//        };
-//        assert_eq!(result, expected);
-//    }
+    #[test]
+    fn test_mutate_phrase() {
+        let mut phrase = Phrase {
+            events: vec![
+                Event::new(100.0, test_ratios(), 1.0, 1.0),
+                Event::new(50.0, test_ratios(), 2.0, 1.0),
+            ],
+        };
 
-//    #[test]
-//    fn test_collapse_phrase() {
-//        let mut phrase1 = Phrase {
-//            events: vec![
-//                Event::new(100.0, test_ratios(), 1.0, 1.0),
-//                Event::new(50.0, test_ratios(), 2.0, 1.0),
-//            ],
-//        };
-//
-//        let phrase2 = phrase1
-//            .clone()
-//            .mut_ratios(test_ratios_change())
-//            .transpose(3.0 / 2.0, 0.0)
-//            .mut_length(2.0, 1.0)
-//            .mut_gain(0.9, 0.0);
-//
-//        let result = vec![phrase1, phrase2].render();
-//
-//        let expected = vec![
-//            Event::new(100.0, test_ratios_change(), 1.0, 1.0),
-//            Event::new(50.0, test_ratios_change(), 2.0, 1.0),
-//            Event::new(150.0, test_ratios_change(), 3.0, 0.9),
-//            Event::new(75.0, test_ratios_change(), 5.0, 0.9),
-//        ].render();
-//
-//        assert_eq!(result, expected);
-//    }
+        let result = phrase
+            .mut_ratios(test_ratios_change())
+            .transpose(3.0 / 2.0, 0.0)
+            .mut_length(2.0, 1.0)
+            .mut_gain(0.9, 0.0);
+
+        let expected = Phrase {
+            events: vec![
+                Event::new(150.0, test_ratios_change(), 3.0, 0.9),
+                Event::new(75.0, test_ratios_change(), 5.0, 0.9),
+            ],
+        };
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_render_phrase() {
+        let mut phrase = Phrase {
+            events: vec![
+                Event::new(100.0, test_ratios(), 1.0, 1.0),
+                Event::new(50.0, test_ratios(), 2.0, 1.0),
+            ],
+        };
+
+        let mut oscillator1 = Oscillator::init(test_ratios(), &get_test_settings());
+        let result = phrase
+            .mut_ratios(test_ratios_change())
+            .transpose(3.0 / 2.0, 0.0)
+            .mut_length(2.0, 1.0)
+            .mut_gain(0.9, 0.0)
+            .render(&mut oscillator1);
+
+        let mut oscillator2 = Oscillator::init(test_ratios(), &get_test_settings());
+        let expected = Phrase {
+            events: vec![
+                Event::new(150.0, test_ratios_change(), 3.0, 0.9),
+                Event::new(75.0, test_ratios_change(), 5.0, 0.9),
+            ],
+        }.render(&mut oscillator2);
+        assert_eq!(result, expected);
+    }
 }
 
