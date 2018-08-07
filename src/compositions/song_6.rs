1940aea55f4aab948516721915c1dc4ad8629275
Danny Meyer <Danny.Meyer@gmail.com>
2018-08-07 12:17:05 -0400

    wip

--- src/compositions/song_6.rs
+++ src/compositions/song_6.rs
@@ -11,7 +11,6 @@ pub fn generate_composition() -> StereoWaveform {
         (3, 2, -3.0, 1.0, 0.0),
         (3, 2, 2.0, 1.0, 0.0),
     ];
-    let e = vec![Event::new(100.0, rs.clone(), 0.11, 1.0)];
 
     let sequence1 = Op::Sequence {
         operations: vec![
@@ -22,8 +21,8 @@ pub fn generate_composition() -> StereoWaveform {
                     r![
                             (1, 1, 0.0, 0.6, 0.3),
                             (1, 1, 0.0, 0.6, -0.3),
-                            (3, 2, -3.0, 1.0, 1.0),
-                            (3, 2, 2.0, 1.0, -1.0),
+                            (3, 2, 3.0, 1.0, 1.0),
+                            (3, 2, -2.0, 1.0, -1.0),
                         ]
                     }
                 ]
@@ -51,7 +50,17 @@ pub fn generate_composition() -> StereoWaveform {
     let sequence2 = Op::Sequence {
         operations: vec![
             Op::Transpose { m: 9.0/8.0, a: 0.0 },
-            Op::AsIs,
+            Op::Compose { operations: vec![
+                    Op::AsIs,
+                        Op::Ratios { ratios:
+                        r![
+                            (1, 1, 2.0, 1.0, 0.2),
+                            (12, 4, -3.0, 1.0, 1.0),
+                            (7, 4, 2.0, 1.0, -1.0),
+                            (2, 1, -0.0, 1.0, -0.2),
+                        ]
+                    }
+                ]},
             Op::Transpose { m: 4.0/3.0, a: 0.0 },
             Op::Transpose { m: 15.0/16.0, a: 0.0 },
 
@@ -73,22 +82,24 @@ pub fn generate_composition() -> StereoWaveform {
             sequence1.clone(),
             sequence2.clone(),
             sequence3.clone(),
-            Op::Sequence {
-                operations: vec![
-                    Op::AsIs,
-                    Op::AsIs,
-                    Op::AsIs,
-                    Op::AsIs,
-                    Op::AsIs,
-                ]
-            }
-        ]
+        Op::Compose {
+            operations: vec![
+                Op::Gain { m: 0.5, a: 0.0 },
+                Op::Sequence {
+                    operations: vec![
+                        Op::AsIs,
+                        Op::AsIs,
+                        Op::AsIs,
+                        Op::AsIs,
+                        Op::AsIs,
+                        Op::AsIs,
+                    ]}
+                ]}
+            ]
     };
 
-//    println!("{:?}", ops.apply(e.clone()));
     let mut oscillator = Oscillator::init(rs.clone(), &get_default_app_settings());
-
-    println!("{:?}", ops3.apply(e.clone()));
-    let mut events = ops3.apply(e.clone());
+    let e = vec![Event::new(100.0, rs.clone(), 0.11, 0.75)];
+    let mut events = ops3.apply(e);
     events.render(&mut oscillator)
 }
