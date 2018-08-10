08e5856efe4d2a00044ec185447be2101f727741
Danny Meyer <Danny.Meyer@gmail.com>
2018-08-10 13:34:53 -0400

    two good fourier tests

--- src/analyze/fourier.rs
+++ src/analyze/fourier.rs
@@ -2,47 +2,60 @@ use std::f32::consts::PI as PI;
 use std::f32::consts::E as E;
 use num_complex::Complex;
 
-pub trait Fourier {
-    fn separate(&mut self);
-    fn fft(&mut self);
-}
+//pub trait Fourier {
+//    fn separate(&mut self);
+//    fn fft(&mut self);
+//}
 
-impl Fourier for Vec<f32> {
-    fn separate(&mut self) {
-        let n = self.len();
-        let mut temp_array = vec![0.0; n / 2];
-        for i in 0..n / 2 {
-            temp_array[i] = self[i * 2 + 1]
-        }
-        for i in 0..n / 2 {
-            self[i] = self[i * 2]
-        }
-        for i in 0..n / 2 {
-            self[i + n / 2] = temp_array[i]
-        }
+//impl Fourier for Vec<f32> {
+pub fn separate(array: &mut Vec<f32>) -> Vec<f32> {
+    let mut a = array.clone();
+    let n = a.len();
+    let mut temp_array = vec![0.0; n / 2];
+    for i in 0..n / 2 {
+        temp_array[i] = a[i * 2 + 1]
+    }
+    for i in 0..n / 2 {
+        a[i] = a[i * 2]
+    }
+    for i in 0..n / 2 {
+        a[i + n / 2] = temp_array[i]
     }
 
-    fn fft(&mut self) {
-        let length = self.len();
-        if length < 2 {}
-        else {
-            self.separate();
-            let mut even = self[0..length / 2].to_vec();
-            even.fft();
-            let mut odd = self[length / 2..length].to_vec();
-            odd.fft();
-
-            for k in 0..length/2 {
-                let e = even[k];
-                let o = odd[k];
-                let complex = Complex::new(0.0, -2.0 * PI * k as f32 / length);
-                let w = E.powf(complex);
-                self[k] = e + w * o;
-                self[length/2 + k] = e - w * o;
-            }
-        }
-    }
+    a
 }
+
+pub fn vec_f32_to_complex(array: &mut Vec<f32>) -> Vec<Complex<f32>> {
+     array.clone()
+        .iter_mut()
+        .map(|x| Complex::new(*x as f32, 0f32))
+        .collect::<Vec<_>>()
+}
+
+//pub fn fft(&mut self) {
+//    let length = self.len();
+//    if length < 2 {}
+//    else {
+//        self.separate();
+//
+//
+//
+//        let mut even = self[0..length / 2].to_vec();
+//        even.fft();
+//        let mut odd = self[length / 2..length].to_vec();
+//        odd.fft();
+//
+//        for k in 0..length/2 {
+//            let e = even[k];
+//            let o = odd[k];
+//            let complex = Complex::new(0.0, -2.0 * PI * k as f32 / length as f32);
+//            let w = complex.exp();
+//            self[k] = e + w * o;
+//            self[length/2 + k] = e - w * o;
+//        }
+//    }
+//}
+////}
 
 //def fft(x):
     //N = len(x)
@@ -61,26 +74,43 @@ pub mod tests {
     use ratios::{R, Pan};
     #[test]
     fn fourier_separate_test() {
-        let array = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
-        let mut result = array.clone();
-        result.separate();
-        let expected = vec![0.0, 2.0, 4.0, 6.0, 8.0, 1.0, 3.0, 5.0, 7.0, 9.0];
+        let mut array = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+        let mut result = separate(&mut array);
+        let expected = vec![0.0, 2.0, 4.0, 6.0, 1.0, 3.0, 5.0, 7.0];
 
         assert_eq!(result, expected);
     }
 
     #[test]
-    fn fourier_fft() {
-//        let n_seconds = 1.0;
-//        let sample_rate = n_samples as f32 / n_seconds;
-//        let freq_resolution = sample_rate / n_samples as f32;
-        let input = vec![1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0];
+    fn fourier_array_to_complex_test() {
+        let mut array = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+        let mut result = vec_f32_to_complex(&mut array);
+        let expected = vec![
+            Complex::new(0.0, 0.0),
+            Complex::new(1.0, 0.0),
+            Complex::new(2.0, 0.0),
+            Complex::new(3.0, 0.0),
+            Complex::new(4.0, 0.0),
+            Complex::new(5.0, 0.0),
+            Complex::new(6.0, 0.0),
+            Complex::new(7.0, 0.0),
+        ];
 
-        let mut result = input.clone();
-        result.fft();
-
-        let expected = vec![4.000, 2.613, 0.000, 1.082, 0.000, 1.082, 0.000, 2.613];
-
-        assert_eq!(expected, result);
+        assert_eq!(result, expected);
     }
+//
+//    #[test]
+//    fn fourier_fft() {
+////        let n_seconds = 1.0;
+////        let sample_rate = n_samples as f32 / n_seconds;
+////        let freq_resolution = sample_rate / n_samples as f32;
+//        let input = vec![1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0];
+//
+//        let mut result = input.clone();
+//        result.fft();
+//
+//        let expected = vec![4.000, 2.613, 0.000, 1.082, 0.000, 1.082, 0.000, 2.613];
+//
+//        assert_eq!(expected, result);
+//    }
 }
