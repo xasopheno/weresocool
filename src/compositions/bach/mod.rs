54dd09c685b0069463ff70751b2893fd6a2ee268
Danny Meyer <Danny.Meyer@gmail.com>
2018-08-11 00:17:34 -0400

    0-24

--- src/compositions/bach/mod.rs
+++ src/compositions/bach/mod.rs
@@ -7,19 +7,25 @@ use settings::get_default_app_settings;
 
 pub fn generate_composition() -> StereoWaveform {
     let rs = r![
+        (1, 1, 0.0, 0.2, 0.0),
         (1, 1, 0.0, 1.0, 0.0),
         (1, 1, 0.0, 1.0, 0.0),
+        (1, 1, 0.0, 1.0, 0.0),
+        (1, 1, 0.0, 1.0, 0.0),
+        (1, 1, 0.0, 1.0, 0.0),
+        (1, 1, 0.0, 0.2, 0.0),
     ];
 
     let sequence1 = Op::Sequence {
         operations: vec![
-            phrases::bach::bach1()
+            phrases::bach::m0_24()
         ],
     };
 
     let mut oscillator = Oscillator::init(rs.clone(), &get_default_app_settings());
-    let e = vec![Event::new(270.0, rs.clone(), 0.2, 1.0)];
+    let e = vec![Event::new(290.0, rs.clone(), 2.0, 1.0)];
     let mut events = sequence1.apply(e);
 
     events.render(&mut oscillator)
+
 }
