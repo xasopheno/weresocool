4ccd19ebff66a451d1431a33555d86aa6eb5759f
Danny Meyer <Danny.Meyer@gmail.com>
2018-08-12 17:19:52 -0400

    wip

--- src/compositions/bach/mod.rs
+++ src/compositions/bach/mod.rs
@@ -35,7 +35,7 @@ pub fn generate_composition() -> StereoWaveform {
     };
 
     let mut oscillator = Oscillator::init(rs.clone(), &get_default_app_settings());
-    let e = vec![Event::new(290.0, rs.clone(), 2.0, 0.5)];
+    let e = vec![Event::new(290.0, rs.clone(), 2.0, 0.35)];
     let mut events = sequence1.apply(e);
 
     events.render(&mut oscillator)
