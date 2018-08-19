69e5c20e6836d29326373df9a6f4be5611f295d6
Danny <danny.meyer@gmail.com>
2018-08-19 18:48:30 -0400

    operation tests for length working

--- src/event/test.rs
+++ src/event/test.rs
@@ -1,25 +1,23 @@
-//mod tests {
-//    use event::{Event};
-//    use ratios::{Pan, R};
-//
-//    fn test_ratios() -> Vec<R> {
-//        r![(1, 1, 0.0, 0.5, 0.0)]
-//    }
-//
-//    #[test]
-//    fn test_event() {
-//        let result = Event::new(100.0, test_ratios(), 0.001, 1.0);
-//
-//        let expected = Event {
-//            frequency: 100.0,
-//            ratios: vec![
-//                R::atio(1, 1, 0.0, 0.25, Pan::Left),
-//                R::atio(1, 1, 0.0, 0.25, Pan::Right),
-//            ],
-//            length: 0.001,
-//            gain: 1.0,
-//        };
-//
-//        assert_eq!(expected, result);
-//    }
-//}
+mod tests {
+    use event::{Event, Sound};
+
+    #[test]
+    fn test_event() {
+        let length = 0.001;
+        let sounds = vec![
+            Sound {
+                frequency: 100.0,
+                gain: 1.0,
+                pan: 1.0
+            }
+        ];
+
+        let result = Event::init(100.0, 1.0, 1.0, 0.001);
+        let expected = Event {
+            sounds: sounds.clone(),
+            length
+        };
+
+        assert_eq!(expected, result);
+    }
+}
--- src/operations/test.rs
+++ src/operations/test.rs
@@ -1,64 +1,106 @@
 pub mod tests {
     use operations::{Op, Operate};
 
-    #[test]
-    fn get_length_test() {
-        let rs = r![(1, 1, 0.0, 1.0, 0.0),];
-
-        let as_is = Op::AsIs {};
-        let transpose_m = Op::TransposeM { m: 1.5 };
-        let transpose_a = Op::TransposeA { a: 1.5 };
-        let gain = Op::Gain { m: 1.5 };
-        let silence = Op::Silence { m: 1.5 };
-        let length = Op::Length { m: 1.5 };
-
-        assert_eq!(as_is.get_length_ratio(), 1.0);
-        assert_eq!(transpose_m.get_length_ratio(), 1.0);
-        assert_eq!(transpose_a.get_length_ratio(), 1.0);
-        assert_eq!(gain.get_length_ratio(), 1.0);
-        assert_eq!(silence.get_length_ratio(), 1.5);
-        assert_eq!(length.get_length_ratio(), 1.5);
-
-        let sequence1 = Op::Sequence {
+    fn sequence1() -> Op {
+        Op::Sequence {
             operations: vec![
                 Op::AsIs,
                 Op::AsIs,
                 Op::TransposeM { m: 2.0 },
                 Op::Length { m: 2.0 },
             ],
-        };
+        }
+    }
+
+    fn sequence2() -> Op {
+        Op::Compose {
+            operations: vec![sequence1(), Op::Length { m: 2.0 }],
+        }
+    }
+
+    fn sequence3() -> Op {
+        Op::Compose {
+            operations: vec![sequence1(), sequence2()],
+        }
+    }
+
+    #[test]
+    fn op_asis_test() {
+        let as_is = Op::AsIs {};
+        assert_eq!(as_is.get_length_ratio(), 1.0);
+    }
+    fn op_reverse_test() {
+        let reverse = Op::Reverse {};
+        assert_eq!(reverse.get_length_ratio(), 1.0);
+    }
+    fn op_pan_test() {
+
+    }
+    fn op_repeat_test() {
+        let sequence1 = sequence1();
         let repeat = Op::Repeat {
             n: 2,
             operations: vec![sequence1.clone()],
         }.get_length_ratio();
 
-        let length1 = sequence1.get_length_ratio();
-        assert_eq!(length1, 5.0);
         assert_eq!(repeat, 10.0);
+    }
+    fn op_length_test() {
+        let length = Op::Length { m: 1.5 };
+        assert_eq!(length.get_length_ratio(), 1.5);
+    }
+    fn op_transpose_m_test() {
+        let transpose_m = Op::TransposeM { m: 1.5 };
+        assert_eq!(transpose_m.get_length_ratio(), 1.0);
+    }
+    fn op_transpose_a_test() {
+        let transpose_a = Op::TransposeA { a: 1.5 };
+        assert_eq!(transpose_a.get_length_ratio(), 1.0);
 
-        let sequence2 = Op::Compose {
-            operations: vec![sequence1.clone(), Op::Length { m: 2.0 }],
-        };
-
-        let length2 = sequence2.clone().get_length_ratio();
-        assert_eq!(length2, 10.0);
-
-        let sequence3 = Op::Compose {
-            operations: vec![sequence1.clone(), sequence2.clone()],
-        };
-
-        let sequence_with_sequence_length = sequence3.clone().get_length_ratio();
-
-        assert_eq!(sequence_with_sequence_length, 50.0);
-
+    }
+    fn op_silence_test() {
+        let silence = Op::Silence { m: 1.5 };
+        assert_eq!(silence.get_length_ratio(), 1.5);
+    }
+    fn op_gain_test() {
+        let gain = Op::Gain { m: 1.5 };
+        assert_eq!(gain.get_length_ratio(), 1.0);
+    }
+    fn op_fit() {
         let fit = Op::Fit {
             n: 1,
-            with_length_of: Box::new(sequence1.clone()),
-            main: Box::new(sequence3.clone()),
+            with_length_of: Box::new(sequence1()),
+            main: Box::new(sequence3()),
         };
 
         let fit_length = fit.get_length_ratio();
 
+
         assert_eq!(fit_length, 5.0);
     }
+    fn op_compose_test() {
+        let sequence_with_sequence_length = sequence3().get_length_ratio();
+        assert_eq!(sequence_with_sequence_length, 50.0);
+    }
+    fn op_sequence_test() {
+        let sequence_length_1 = sequence1().get_length_ratio();
+        let sequence_length_2 = sequence2().get_length_ratio();
+        assert_eq!(sequence_length_1, 5.0);
+        assert_eq!(sequence_length_2, 10.0);
+    }
+    fn op_overlay_test() {
+        let overlay = Op::Overlay { operations: vec![
+            sequence1(),
+            sequence2(),
+            sequence3()
+        ]};
+        assert_eq!(overlay.get_length_ratio(), 50.0);
+    }
+
+//    #[test]
+//    fn apply_test() {
+//
+//
+//        assert_eq!(fit_length, 5.0);
+//    }
 }
