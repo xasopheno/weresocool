04c9658ff03a8212e59f146b8271bf8c9cee6e80
Danny Meyer <Danny.Meyer@gmail.com>
2018-09-02 18:59:13 -0400

    high note

--- src/compositions/song_15/area3.rs
+++ src/compositions/song_15/area3.rs
@@ -3,6 +3,8 @@ pub mod material3 {
 
     fn overtones() -> Op {
         r![
+            (8, 1, 5.0, 0.1, 0.8),
+            (8, 1, 5.0, 0.1, -0.8),
             (2, 1, 5.0, 0.25, 0.8),
             (2, 1, 0.0, 0.25, -0.8),
             (3, 1, 7.0, 0.1, 0.4),
@@ -15,7 +17,7 @@ pub mod material3 {
     pub fn s5_melody() -> Op {
         Op::Compose { operations: vec![
             Op::Sequence { operations: vec![
-                Op::AsIs,
+//                Op::AsIs,
             ]}
         ]}
     }
@@ -24,7 +26,7 @@ pub mod material3 {
         Op::Compose { operations: vec![
             Op::Sequence {
                 operations: vec![
-                    Op::AsIs,
+                    Op::TransposeM {m: 1.0/1.0},
                     Op::TransposeM {m: 9.0/8.0},
                     Op::TransposeM {m: 6.0/5.0},
                     Op::TransposeM {m: 9.0/8.0},
