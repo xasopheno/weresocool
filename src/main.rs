6781483eb82dab14f9c7bac1c807cd249182f2a8
Danny Meyer <Danny.Meyer@gmail.com>
2018-10-08 21:14:24 -0400

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-#[macro_use] extern crate lalrpop_util;
+#[macro_use]
+extern crate lalrpop_util;
 lalrpop_mod!(pub socool); // synthesized by LALRPOP
 pub mod ast;
 
 fn main() {
-    println!("{:?}", socool::OperationParser::new().parse(
-        "o[(3/2, 3.0, 1.0, 0.0),
+    println!(
+        "{:?}",
+        socool::OperationParser::new().parse(
+            "o[(3/2, 3.0, 1.0, 0.0),
            (3/2, 0.0, 1.0, 0.0),
            (1, 0.0, 1.0, 0.0)]"
-    ));
-//    println!("{:?}", socool::OperationParser::new().parse(
-//        "Tm 3/2
-//        | Gain 0.5"
-//    ));
-//    println!("{:?}", socool::OperationParser::new().parse(
-//        "Tm 3/2"
-//    ));
+        )
+    );
+    //    println!("{:?}", socool::OperationParser::new().parse(
+    //        "Tm 3/2
+    //        | Gain 0.5"
+    //    ));
+    //    println!("{:?}", socool::OperationParser::new().parse(
+    //        "Tm 3/2"
+    //    ));
 }
 
 #[cfg(test)]
@@ -23,30 +27,72 @@ mod tests {
     lalrpop_mod!(pub socool); // synthesized by LALRPOP
     #[test]
     fn ops() {
-        assert_eq!(socool::OperationParser::new().parse("  Tm 3/2").unwrap(), Op::TransposeM {m: 1.5});
-        assert_eq!(socool::OperationParser::new().parse("Ta 3.0").unwrap(), Op::TransposeA {a: 3.0});
-        assert_eq!(socool::OperationParser::new().parse("PanM   3.0").unwrap(), Op::PanM {m: 3.0});
-        assert_eq!(socool::OperationParser::new().parse("PanA 3.0").unwrap(), Op::PanA {a: 3.0});
-        assert_eq!(socool::OperationParser::new().parse("Gain 3.0").unwrap(), Op::Gain {m: 3.0});
-        assert_eq!(socool::OperationParser::new().parse("Length 3.0").unwrap(), Op::Length {m: 3.0});
-        assert_eq!(socool::OperationParser::new().parse("Reverse").unwrap(), Op::Reverse);
-        assert_eq!(socool::OperationParser::new().parse("AsIs").unwrap(), Op::AsIs);
-        assert_eq!(socool::OperationParser::new().parse("
-            Sequence [
-                AsIs,
-                Tm 3/2,
-            ]").unwrap(), Op::Sequence { operations: vec![Op::AsIs, Op::TransposeM {m: 3.0/2.0}]}
+        assert_eq!(
+            socool::OperationParser::new().parse("  Tm 3/2").unwrap(),
+            Op::TransposeM { m: 1.5 }
         );
-        assert_eq!(socool::OperationParser::new().parse("
+        assert_eq!(
+            socool::OperationParser::new().parse("Ta 3.0").unwrap(),
+            Op::TransposeA { a: 3.0 }
+        );
+        assert_eq!(
+            socool::OperationParser::new().parse("PanM   3.0").unwrap(),
+            Op::PanM { m: 3.0 }
+        );
+        assert_eq!(
+            socool::OperationParser::new().parse("PanA 3.0").unwrap(),
+            Op::PanA { a: 3.0 }
+        );
+        assert_eq!(
+            socool::OperationParser::new().parse("Gain 3.0").unwrap(),
+            Op::Gain { m: 3.0 }
+        );
+        assert_eq!(
+            socool::OperationParser::new().parse("Length 3.0").unwrap(),
+            Op::Length { m: 3.0 }
+        );
+        assert_eq!(
+            socool::OperationParser::new().parse("Reverse").unwrap(),
+            Op::Reverse
+        );
+        assert_eq!(
+            socool::OperationParser::new().parse("AsIs").unwrap(),
+            Op::AsIs
+        );
+        assert_eq!(
+            socool::OperationParser::new()
+                .parse("
+                Sequence [
+                    AsIs,
+                    Tm 3/2,
+                ]
+                ")
+                .unwrap(),
+            Op::Sequence {
+                operations: vec![Op::AsIs, Op::TransposeM { m: 3.0 / 2.0 }]
+            }
+        );
+        assert_eq!(
+            socool::OperationParser::new()
+                .parse("
                 Overlay [
                     AsIs,
                     Tm 3/2,
-                ]").unwrap(), Op::Overlay { operations: vec![Op::AsIs, Op::TransposeM {m: 3.0/2.0}]}
+                ]
+                ")
+                .unwrap(),
+            Op::Overlay {
+                operations: vec![Op::AsIs, Op::TransposeM { m: 3.0 / 2.0 }]
+            }
         );
-        assert!(socool::OperationParser::new().parse(
-            "o[(3/2, 3.0, 1.0, 0.0),
-               (3/2, 0.0, 1.0, 0.0),
-               (1, 0.0, 1.0, 0.0)]").is_ok()
+        assert!(
+            socool::OperationParser::new()
+                .parse(
+                    "o[(3/2, 3.0, 1.0, 0.0),
+                       (3/2, 0.0, 1.0, 0.0),
+                       (1, 0.0, 1.0, 0.0)]"
+                )
+                .is_ok()
         )
     }
 }
