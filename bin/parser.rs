40f290b70b43f0a1145327ceef32f555a6beadbe
Danny Meyer <Danny.Meyer@gmail.com>
2018-10-13 13:49:51 -0400

    adds plan

--- bin/parser.rs
+++ bin/parser.rs
@@ -8,11 +8,35 @@ use std::env;
 fn main() -> Result<(), pa::Error> {
     println!("{}", "\n  ****** WereSoCool __!Now In Stereo!__ ****** ");
     println!("{}", "*** Make cool sounds. Impress your friends ***  ");
-    println!("{}", "       )))***=== COMPOSER ===***(((  \n ");
     println!("{}", " ~~~~“Catchy tunes for your next seizure.”~~~~");
 
+//    TODO: this will take the filename as an arg, pass it to the parser
+//    TODO: after a successful parsed:
+//
+//      TODO: Init{f, l, g, p} will be passed to Event::new(f, g, p, l)
+    //    TODO: init oscillator
+
+        //fn event() -> Event {
+        //    Event::init(400.0, 0.75, 0.0, 0.5)
+        //}
+
+//      TODO: Operations
+//      operations = ParseTable.get("main")
+
+//      TODO: generate events
+//      let events = operation().apply(vec![event])
+
+        // TODO: init oscillator
+        //  fn oscillator() -> Oscillator {
+        //  Oscillator::init(&get_default_app_settings())
+        //}
+
+        //pub fn generate_composition() -> StereoWaveform {
+        //    events().render(&mut oscillator())
+        //}
+
     let pa = pa::PortAudio::new()?;
-    let mut output_stream = setup_portaudio_output(generate_composition, &pa)?;
+    let mut output_stream = setup_portsaudio_output(generate_composition, &pa)?;
     output_stream.start()?;
 
     while let true = output_stream.is_active()? {}
