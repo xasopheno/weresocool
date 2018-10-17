6a4837f7f159c6ce2f6d0ed8c3382f34e66e157b
Danny Meyer <Danny.Meyer@gmail.com>
2018-10-17 12:31:07 -0400

    adds print arg

--- bin/parser.rs
+++ bin/parser.rs
@@ -24,30 +24,40 @@ fn main() -> Result<(), pa::Error> {
                  .help("filename eg: my_song.socool")
                  .required(false)
         )
+        .arg(Arg::with_name("print")
+            .help("Prints file to .wav")
+            .short("p")
+            .long("print"))
         .get_matches();
+    
+        let filename = matches.value_of("filename");
+        match filename {
+                Some(_filename) => {},
+                _ => {        println!("\n{}\n", "Forgot to pass in a filename.".red().bold());
+                println!("{}", "Example:".cyan());
+                println!("{}\n", "./weresocool song.socool".cyan().italic());
+                panic!("Wrong number of arguments.")
+            }
 
-    let filename = matches.value_of("filename");
-    match filename {
-            Some(_filename) => {},
-            _ => {        println!("\n{}\n", "Forgot to pass in a filename.".red().bold());
-            println!("{}", "Example:".cyan());
-            println!("{}\n", "./weresocool song.socool".cyan().italic());
-            panic!("Wrong number of arguments.")
         }
 
+        let parsed = parse_file(&filename.unwrap().to_string());
+        let main = parsed.table.get("main").unwrap();
+        let init = parsed.init;
+
+    if matches.is_present("print") {
+        println!("\n        Printing: {}\n", &filename.unwrap().to_string());
+        println!("{}", "This would print instead of run");
+    } else {
+        println!("\n        Now Playing: {}\n", &filename.unwrap().to_string());
+        let pa = pa::PortAudio::new()?;
+        let mut output_stream = setup_portaudio_output(generate_composition(init, main.clone()), &pa)?;
+        output_stream.start()?;
+
+        while let true = output_stream.is_active()? {}
+
+        output_stream.stop()?;
     }
-
-    let parsed = parse_file(&filename.unwrap().to_string());
-    let main = parsed.table.get("main").unwrap();
-    let init = parsed.init;
-
-    let pa = pa::PortAudio::new()?;
-    let mut output_stream = setup_portaudio_output(generate_composition(init, main.clone()), &pa)?;
-    output_stream.start()?;
-
-    while let true = output_stream.is_active()? {}
-
-    output_stream.stop()?;
 
     Ok(())
 }
