aef764c510e09099d0f3d39f72efc322dba47eeb
Danny Meyer <Danny.Meyer@gmail.com>
2018-12-25 22:34:17 -0700

    Factors out tests from string stuff in pad

--- bin/pad.rs
+++ bin/pad.rs
@@ -1,18 +1,7 @@
 extern crate weresocool;
-use std::collections::HashMap;
 
 fn main() {
     println!("\nHello Danny's WereSoCool Scratch Pad");
-
-    let mut hm: HashMap<String, usize> = HashMap::new();
-    hm.insert("this.thing".to_string(), 1);
-    println!("{:#?}", hm);
-    let result = hm.get(&"this.thing".to_string());
-    println!("{:#?}", result);
-    assert_eq!(result, Some(&1));
-    let nope = hm.get(&"this".to_string());
-    println!("{:?}", nope);
-    assert_eq!(nope, None);
 
     let string_list: Vec<String> = vec!["this", "thing"]
         .iter_mut()
@@ -22,57 +11,55 @@ fn main() {
     let joined = string_list.join(".");
     println!("{:#?}", joined);
     assert_eq!(joined, "this.thing".to_string());
+}
 
+fn is_import(s: String) -> bool {
+    s.trim().starts_with("import")
+}
+
+fn is_as_import(s: String) -> bool {
+    s.trim();
+    s.contains(" as ")
+}
+
+fn test_data() -> Vec<String> {
     let import_str = "   import standard ".to_string();
     let import_as_str = "import  standard as std".to_string();
     let not_import_as_str = "import standardasstd  ".to_string();
     let not_import = "not an import".to_string();
-    let lines = vec![import_str, import_as_str, not_import_as_str, not_import];
+    vec![import_str, import_as_str, not_import_as_str, not_import]
+}
 
-    fn is_import(s: String) -> bool {
-       s.trim().starts_with("import")
-    };
+#[test]
+fn test_import_strings() {
+    let lines = test_data();
+    let starts_with_import: Vec<bool> = lines
+        .iter()
+        .map(|line| is_import(line.to_string()))
+        .collect();
 
-    fn is_as_import(s: String) -> bool {
-        s.trim();
-        s.contains(" as ")
-    }
+    let is_as_import: Vec<bool> = lines
+        .iter()
+        .map(|line| is_as_import(line.to_string()))
+        .collect();
 
-    #[test]
-    fn test_import_strings() {
-        let starts_with_import: Vec<bool> =
-            lines
-                .iter()
-                .map(|line| is_import(line.to_string()))
-                .collect();
+    assert_eq!(starts_with_import, vec![true, true, true, false]);
+    assert_eq!(is_as_import, vec![false, true, false, false]);
+}
 
-        let is_as_import: Vec<bool> =
-            lines
-                .iter()
-                .map(|line| is_as_import(line.to_string()))
-                .collect();
-
-        assert_eq!(starts_with_import, vec![true, true, true, false]);
-        assert_eq!(is_as_import, vec![false, true, false, false]);
-    }
-
-    let w_o_import: Vec<String> =
-        lines
-            .iter()
-            .filter(|line| is_import(line.to_string()))
-            .map(|line| {
-                line
-                    .replace("import ", "")
-            })
-            .collect();
+#[test]
+fn test_import_strings2() {
+    let lines = test_data();
+    let w_o_import: Vec<String> = lines
+        .iter()
+        .filter(|line| is_import(line.to_string()))
+        .map(|line| line.replace("import ", ""))
+        .collect();
 
     let trimmed: Vec<String> = w_o_import
         .iter()
         .map(|line| line.trim().to_string())
         .collect();
-    println!("{:#?}", trimmed);
 
-
-
-
+    assert_eq!(trimmed, vec!["standard", "standard as std", "standardasstd"])
 }
