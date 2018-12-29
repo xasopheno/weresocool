9b61aa7121bb91d40391e602905ed9c1a24aaf35
Danny Meyer <Danny.Meyer@gmail.com>
2018-12-28 20:32:02 -0800

    import test passing

--- src/generation/test.rs
+++ src/generation/test.rs
@@ -5,7 +5,8 @@ mod parse_tests {
     fn import_test() {
         let filename = &"songs/test/import_test.socool".to_string();
         let parsed = parse_file(filename, None);
-        let mut result: Vec<String> = parsed.table
+        let mut result: Vec<String> = parsed
+            .table
             .iter()
             .map(|(name, _)| name.to_string())
             .collect();
@@ -13,18 +14,18 @@ mod parse_tests {
         result.sort();
 
         let mut expected: Vec<String> = vec![
-            "std_test.fade_out",
-            "std_test.main",
+            "danny",
             "import_test_2.main",
-            "import_test_2.std_test.fade_out",
             "import_test_2.root_fifth",
+            "import_test_2.std_test.fade_out",
             "import_test_2.std_test.main",
+            "main",
             "standard.fade_out",
+            "standard.import_test_2.main",
             "standard.import_test_2.root_fifth",
             "standard.import_test_2.std_test.fade_out",
-            "standard.main",
-            "standard.import_test_2.main",
             "standard.import_test_2.std_test.main",
+            "standard.main",
         ]
         .iter()
         .map(|s| s.to_string())
@@ -32,6 +33,6 @@ mod parse_tests {
 
         expected.sort();
 
-        assert_eq!(result, expected)
+        assert_eq!(expected, result)
     }
 }
