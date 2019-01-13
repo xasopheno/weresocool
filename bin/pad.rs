9ae237ccf6d3794431fc51c1a630cfbf04c97b0f
Danny Meyer <Danny.Meyer@gmail.com>
2019-01-13 16:12:18 -0500

    wip

--- bin/pad.rs
+++ bin/pad.rs
@@ -5,7 +5,7 @@ use num_rational::{Ratio, Rational64};
 use socool_parser::ast::Op::*;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
-use weresocool::instrument::oscillator::OscType::Sine;
+use weresocool::instrument::oscillator::OscType::{Sine, Noise};
 use weresocool::operations::NormalForm;
 use weresocool::operations::PointOp;
 
@@ -45,8 +45,12 @@ fn test_point_op() {
     c.g = Rational64::new(3, 2);
     c.fa = Rational64::new(2, 1);
 
+    let mut d = PointOp::init();
+    c.osc_type = Noise;
+    c.pa = Rational64::new(1, 1);
+
     let mut nf_a = vec![vec![a.clone(), a.clone()]];
-    let mut nf_b = vec![vec![b.clone(), b.clone(), c.clone()]];
+    let mut nf_b = vec![vec![b.clone(), c.clone(), d.clone()]];
 
     let mut new_nf = vec![];
     for vec_a in nf_a.iter() {
@@ -54,7 +58,7 @@ fn test_point_op() {
             for vec_b in nf_b.iter() {
                 let mut new_vec: Vec<PointOp> = vec![];
                 for point_op_b in vec_b {
-                    new_vec.push(point_op_a.clone() * point_op_b.clone())
+                    new_vec.push(point_op_b.clone() * point_op_a.clone())
                 }
                 new_nf.push(new_vec)
             }
@@ -82,6 +86,15 @@ fn test_point_op() {
                 l: Rational64::new(2, 1),
                 osc_type: Sine,
             },
+            PointOp {
+                fm: Rational64::new(1, 1),
+                fa: Rational64::new(2, 1),
+                pm: Rational64::new(1, 1),
+                pa: Rational64::new(0, 1),
+                g: Rational64::new(3, 2),
+                l: Rational64::new(1, 1),
+                osc_type: Sine,
+            },
         ],
         vec![
             PointOp {
@@ -102,24 +115,13 @@ fn test_point_op() {
                 l: Rational64::new(2, 1),
                 osc_type: Sine,
             },
-        ],
-        vec![
             PointOp {
-                fm: Rational64::new(5,4),
-                fa: Rational64::new(0,1),
-                pm: Rational64::new(1,1),
-                pa: Rational64::new(0,1),
-                g: Rational64::new(1,1),
-                l: Rational64::new(2,1),
-                osc_type: Sine,
-            },
-            PointOp {
-                fm: Rational64::new(1,1),
-                fa: Rational64::new(2,1),
-                pm: Rational64::new(1,1),
-                pa: Rational64::new(0,1),
-                g: Rational64::new(3,2),
-                l: Rational64::new(1,1),
+                fm: Rational64::new(1, 1),
+                fa: Rational64::new(2, 1),
+                pm: Rational64::new(1, 1),
+                pa: Rational64::new(0, 1),
+                g: Rational64::new(3, 2),
+                l: Rational64::new(1, 1),
                 osc_type: Sine,
             },
         ],
