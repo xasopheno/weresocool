d1c248c6e8f1951f6338c78edbe4f7f7e221915d
Danny Meyer <Danny.Meyer@gmail.com>
2019-01-22 01:04:10 -0500

    wip

--- bin/pad.rs
+++ bin/pad.rs
@@ -41,7 +41,7 @@ fn main() {
     if test_table == decoded {
         println!("All Tests Passed");
     } else {
-        show_difference(test_table, decoded);
+        show_difference(decoded, test_table);
         println!("Error");
     }
     //    assert_eq!(test_table, decoded)
@@ -167,7 +167,7 @@ fn generate_render_hashes(p: &String) -> CompositionHashes {
     result.normalize();
 
     let render_hash = sum_vec(result.l_buffer) + sum_vec(result.r_buffer);
-    let render_hash = (render_hash * 1_000_000_000_000.0).round() / 1_000_000_000_000.0;
+    let render_hash = (render_hash * 100_000_000_000_000.0).round() / 100_000_000_000_000.0;
 
     let hashes = CompositionHashes {
         op: op_hash,
