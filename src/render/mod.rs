630680ebb868dd7622d74cb303faeeb1c73c856c
Danny Meyer <Danny.Meyer@gmail.com>
2019-02-19 01:08:43 -0500

    maybe

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -26,8 +26,22 @@ impl Render<Vec<PointOp>> for Vec<PointOp> {
         let mut result: StereoWaveform = StereoWaveform::new(0);
         let mut p_ops = self.clone();
         p_ops.push(PointOp::init_silent());
-        for mut p_op in p_ops {
-            let stereo_waveform = p_op.render(origin, oscillator);
+
+        let mut iter = p_ops.iter().peekable();
+
+
+        while let Some(p_op) = iter.next() {
+            let pk = iter.peek();
+            match pk {
+                Some(p) => {
+                    if p.g == Rational64::new(0,1) || p.fa == Rational64::new(0,1) {
+                        println!("{:?}", p.fm);
+                    }
+                },
+                None => {}
+            }
+
+            let stereo_waveform = p_op.clone().render(origin, oscillator);
             result.append(stereo_waveform);
         }
 
