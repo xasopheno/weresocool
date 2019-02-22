6d30ce6ba717668ff29b4212a916720579b24310
Danny Meyer <Danny.Meyer@gmail.com>
2019-02-21 23:03:04 -0500

    wip

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,48 +1,30 @@
-extern crate num_rational;
 extern crate socool_ast;
 use generation::parsed_to_render::r_to_f64;
 use instrument::{
     oscillator::{Origin, Oscillator},
     stereo_waveform::StereoWaveform,
 };
-use num_rational::Rational64;
 use socool_ast::operations::PointOp;
 
 pub trait Render<T> {
-    fn render(&mut self, origin: &Origin, oscillator: &mut Oscillator, silence_next: bool) -> StereoWaveform;
+    fn render(&mut self, origin: &Origin, oscillator: &mut Oscillator) -> StereoWaveform;
 }
 
 impl Render<PointOp> for PointOp {
-    fn render(&mut self, origin: &Origin, oscillator: &mut Oscillator, silence_next: bool) -> StereoWaveform {
-        oscillator.update(origin.clone(), self);
+    fn render(&mut self, origin: &Origin, oscillator: &mut Oscillator) -> StereoWaveform {
+        oscillator.update(*origin, self);
         let n_samples_to_generate = r_to_f64(self.l) * origin.l * 44_100.0;
-
         oscillator.generate(n_samples_to_generate)
     }
 }
 
 impl Render<Vec<PointOp>> for Vec<PointOp> {
-    fn render(&mut self, origin: &Origin, oscillator: &mut Oscillator, _silence_next: bool) -> StereoWaveform {
+    fn render(&mut self, origin: &Origin, oscillator: &mut Oscillator) -> StereoWaveform {
         let mut result: StereoWaveform = StereoWaveform::new(0);
         let mut p_ops = self.clone();
         p_ops.push(PointOp::init_silent());
-
-        let mut iter = p_ops.iter().peekable();
-
-        while let Some(p_op) = iter.next() {
-            let pk = iter.peek();
-            let silence_next = match pk {
-                Some(p) => {
-                    if p.g == Rational64::new(0,1) || p.fm == Rational64::new(0,1) {
-                        true
-                    } else {
-                        false
-                    }
-                },
-                None => { true }
-            };
-
-            let stereo_waveform = p_op.clone().render(origin, oscillator, silence_next);
+        for mut p_op in p_ops {
+            let stereo_waveform = p_op.render(origin, oscillator);
             result.append(stereo_waveform);
         }
 
