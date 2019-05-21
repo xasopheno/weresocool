65624b44014735477a22224c0fd680f3f353397b
Danny Meyer <Danny.Meyer@gmail.com>
2019-05-20 22:28:29 -0600

    uses filename to nfandbasis in mic

--- bin/mic.rs
+++ bin/mic.rs
@@ -2,6 +2,7 @@ use socool_ast::{NormalForm, Normalize};
 use socool_parser::parse_file;
 use weresocool::{
     examples::documentation,
+    generation::{filename_to_render, RenderReturn, RenderType},
     portaudio::duplex_setup,
     ui::{get_args, no_file_name, were_so_cool_logo},
 };
@@ -33,10 +34,10 @@ fn run() -> Result<(), pa::Error> {
         _ => no_file_name(),
     }
 
-    let parsed = parse_file(&filename.unwrap().to_string(), None);
-    let main = parsed.table.get("main").unwrap();
-
-    let mut normal_form = NormalForm::init();
+    let mut normal_form = match filename_to_render(filename.unwrap(), RenderType::NfAndBasis) {
+        RenderReturn::NfAndBasis(nf, basis) => nf,
+        _ => panic!("Error. Unable to generate NormalForm"),
+    };
 
     println!("\nGenerating Composition ");
     main.apply_to_normal_form(&mut normal_form, &parsed.table);
