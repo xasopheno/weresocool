b5f8349498e26d1f422632a21e8c290839b66dd5
Danny Meyer <Danny.Meyer@gmail.com>
2019-07-03 11:26:45 -0400

    wip

--- bin/pad.rs
+++ bin/pad.rs
@@ -1,23 +1,15 @@
-use weresocool::generation::{
-    OpCsv1d,
-    Op4D,
-    NormalizerJson,
-};
+use weresocool::generation::{NormalizerJson, Op4D, OpCsv1d};
 
-use std::{
-    fs::{File},
-};
+use std::fs::File;
 
-use serde_json::from_reader;
 use csv;
+use serde_json::from_reader;
 
 fn main() {
     let file = File::open("renders/alex.socool.csv").unwrap();
-    let normalizer = File::open("songs/normalizers/alex.socool.normalizer")
-        .unwrap();
+    let normalizer = File::open("songs/normalizers/alex.socool.normalizer").unwrap();
     let normalizer_json: NormalizerJson = from_reader(&normalizer).unwrap();
     println!("{:?}", normalizer);
-    
 
     let mut rdr = csv::Reader::from_reader(file);
     for result in rdr.deserialize() {
