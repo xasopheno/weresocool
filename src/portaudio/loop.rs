6305fbdf17b335e068748097b52ed8cbd860e3dd
Danny Meyer <Danny.Meyer@gmail.com>
2019-10-12 22:49:18 -0600

    ready to fix duplex

--- src/portaudio/loop.rs
+++ src/portaudio/loop.rs
@@ -12,7 +12,8 @@ use std::iter::Cycle;
 use std::vec::IntoIter;
 
 pub struct RealTimeState {
-    count: Rational64, inc: Rational64,
+    count: Rational64,
+    inc: Rational64,
     current_op: PointOp,
 }
 
@@ -142,36 +143,36 @@ pub fn duplex_setup(
     Ok(duplex_stream)
 }
 
-//fn get_duplex_settings(
-    //ref pa: &pa::PortAudio,
-    //ref settings: &Settings,
-//) -> Result<pa::stream::DuplexSettings<f32, f32>, Error> {
-    //let def_input = pa.default_input_device()?;
-    //let input_info = pa.device_info(def_input)?;
-    ////    println!("Default input device info: {:#?}", &input_info);
+fn get_duplex_settings(
+    ref pa: &pa::PortAudio,
+    ref settings: &Settings,
+) -> Result<pa::stream::DuplexSettings<f32, f32>, Error> {
+    let def_input = pa.default_input_device()?;
+    let input_info = pa.device_info(def_input)?;
+    //    println!("Default input device info: {:#?}", &input_info);
 
-    //let latency = input_info.default_low_input_latency;
-    //let input_params = pa::StreamParameters::<f32>::new(
-        //def_input,
-        //settings.channels,
-        //settings.interleaved,
-        //latency,
-    //);
+    let latency = input_info.default_low_input_latency;
+    let input_params = pa::StreamParameters::<f32>::new(
+        def_input,
+        settings.channels,
+        settings.interleaved,
+        latency,
+    );
 
-    //let def_output = pa.default_output_device()?;
-    //let output_info = pa.device_info(def_output)?;
-    ////    println!("Default output device info: {:#?}", &output_info);
+    let def_output = pa.default_output_device()?;
+    let output_info = pa.device_info(def_output)?;
+    //    println!("Default output device info: {:#?}", &output_info);
 
-    //let latency = output_info.default_low_output_latency;
-    //let output_params =
-        //pa::StreamParameters::new(def_output, settings.channels, settings.interleaved, latency);
+    let latency = output_info.default_low_output_latency;
+    let output_params =
+        pa::StreamParameters::new(def_output, settings.channels, settings.interleaved, latency);
 
-    //let duplex_settings = pa::DuplexStreamSettings::new(
-        //input_params,
-        //output_params,
-        //settings.sample_rate as f64,
-        //settings.buffer_size as u32,
-    //);
+    let duplex_settings = pa::DuplexStreamSettings::new(
+        input_params,
+        output_params,
+        settings.sample_rate as f64,
+        settings.buffer_size as u32,
+    );
 
-    //Ok(duplex_settings)
-//}
+    Ok(duplex_settings)
+}
