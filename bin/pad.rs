3ad25f444efa0d04b8cc7674adf51389733de806
Danny Meyer <Danny.Meyer@gmail.com>
2019-10-14 21:51:59 -0600

    pad ready to write work handler

--- bin/pad.rs
+++ bin/pad.rs
@@ -1,55 +1,32 @@
-//use socool_ast::{NormalForm, PointOp};
-//use weresocool::{
-//examples::documentation,
-//generation::{
-//composition_to_vec_timed_op, filename_to_render, RenderReturn, RenderType, TimedOp,
-//},
-//instrument::{Basis, Oscillator},
-////portaudio::live::{live_setup, LiveState},
-//settings::{default_settings, Settings},
-//ui::{get_args, no_file_name, were_so_cool_logo},
-//};
-
-//use error::Error;
-//use failure::Fail;
+use error::Error;
+use failure::Fail;
 //use num_rational::Rational64;
+//use socool_ast::{NormalForm, PointOp};
+use weresocool::generation::{
+    filename_to_render, parsed_to_render::r_to_f64, renderable::nf_to_vec_renderable, RenderReturn,
+    RenderType,
+};
 
 fn main() {
-    //match run() {
-    //Ok(_) => {}
-    //e => {
-    //for cause in Fail::iter_causes(&e.unwrap_err()) {
-    //println!("Failure caused by: {}", cause);
-    //}
-    //}
-    //}
+    match run() {
+        Ok(_) => {}
+        e => {
+            for cause in Fail::iter_causes(&e.unwrap_err()) {
+                println!("Failure caused by: {}", cause);
+            }
+        }
+    }
 }
 
-//fn run() -> Result<(), Error> {
-//let args = get_args();
+#[allow(unused_variables)]
+fn run() -> Result<(), Error> {
+    let (nf, basis, table) =
+        match filename_to_render("songs/test/live.socool", RenderType::NfBasisAndTable)? {
+            RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
+            _ => panic!("Error. Unable to generate NormalForm"),
+        };
+    let renderables = nf_to_vec_renderable(&nf, &table, &basis);
+    let basis_f = r_to_f64(basis.f);
 
-//let filename = args.value_of("filename");
-//match filename {
-//Some(_filename) => {}
-//_ => no_file_name(),
-//}
-
-//let (normal_form, basis, table) =
-//match filename_to_render(filename.unwrap(), RenderType::NfBasisAndTable)? {
-//RenderReturn::NfAndBasis(nf, basis, table) => (nf, basis, table),
-//_ => panic!("Error. Unable to generate NormalForm"),
-//};
-
-//let (vec_timed_op, n_voices) = composition_to_vec_timed_op(&normal_form, &table);
-//let settings = default_settings();
-//let mut live_state = LiveState::new(vec_timed_op, n_voices, basis, &settings);
-
-//live_state.render_batch();
-//let mut live_stream = live_setup(live_state)?;
-//live_stream.start()?;
-
-//while let true = live_stream.is_active()? {}
-
-//live_stream.stop()?;
-//Ok(())
-//}
+    Ok(())
+}
