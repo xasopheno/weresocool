3d6f3788fc70c1da6ce83c4f5d271dc6ca7db16b
Danny Meyer <Danny.Meyer@gmail.com>
2019-11-10 01:32:54 -0500

    rehash

--- src/testing/mod.rs
+++ src/testing/mod.rs
@@ -85,7 +85,7 @@ fn generate_render_hashes(p: &String) -> CompositionHashes {
     result.normalize();
 
     let render_hash = sum_vec(result.l_buffer) + sum_vec(result.r_buffer);
-    let render_hash = (render_hash * 1_000_000_000_000.0).ceil() / 1_000_000_000_000.0;
+    let render_hash = (render_hash * 10_000_000_000_000.0).ceil() / 10_000_000_000_000.0;
     let render_hash_string = &render_hash.to_string()[..13];
 
     let hashes = CompositionHashes {
