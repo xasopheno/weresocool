1a4c1809c1f3a7ed85a9ecd56c23770ba299bf5f
Danny Meyer <Danny.Meyer@gmail.com>
2019-12-18 13:04:10 -0700

    WIP

--- bin/pad.rs
+++ bin/pad.rs
@@ -19,40 +19,35 @@ fn main() {
 #[allow(unused_variables)]
 fn run() -> Result<(), Error> {
     let stereo_waveform =
-        match filename_to_render("songs/alaska/day_6.socool", RenderType::StereoWaveform)? {
+        match filename_to_render("songs/fall/table.socool", RenderType::StereoWaveform)? {
             RenderReturn::StereoWaveform(sw) => sw,
             _ => panic!("Error. Unable to return StereoWaveform"),
         };
     let len = &stereo_waveform.l_buffer.len();
-    //let offset = 0.001;
-    //for (i, sample) in stereo_waveform.l_buffer.iter_mut().enumerate() {
-    //if i < len / 2 {
-    //*sample -= offset;
-    //}
-    //}
-    //for (i, sample) in stereo_waveform.r_buffer.iter_mut().enumerate() {
-    //if i < len / 2 {
-    //*sample -= offset;
-    //}
-    //}
-    let mut max = 0.0;
-    let mut max_i = 0;
+
+    let mut max_d = 0.0;
+    let mut max_v = 0.0;
+
     for (i, sample) in stereo_waveform.r_buffer.clone().iter_mut().enumerate() {
         if i > 0 {
             let d = &stereo_waveform.r_buffer[i] - &stereo_waveform.r_buffer[i - 1];
-            if d.abs() > max {
-                max = d.abs();
-                max_i = i;
+            let v = &stereo_waveform.r_buffer[i - 1];
+            if d.abs() > max_d {
+                max_d = d.abs();
+            }
+            if v.abs() > max_v {
+                max_v = v.abs();
             }
         }
     }
     dbg!(&len);
-    dbg!(max);
-    dbg!(max_i);
-    let mut output_stream = output_setup(stereo_waveform)?;
+    dbg!(max_d);
+    dbg!(max_v);
+    //TODO: Seems like any max distance over ~0.2 is an error.
+    //let mut output_stream = output_setup(stereo_waveform)?;
 
-    output_stream.start()?;
-    while let true = output_stream.is_active()? {}
-    output_stream.stop()?;
+    //output_stream.start()?;
+    //while let true = output_stream.is_active()? {}
+    //output_stream.stop()?;
     Ok(())
 }
