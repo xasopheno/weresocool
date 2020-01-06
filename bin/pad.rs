d683f803e3481942dc2b0e59fc2ad979308191b3
Danny Meyer <Danny.Meyer@gmail.com>
2020-01-05 23:25:34 -0500

    test that file and language same

--- bin/pad.rs
+++ bin/pad.rs
@@ -1,5 +1,8 @@
 use error::Error;
 use failure::Fail;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     match run() {
@@ -12,7 +15,39 @@ fn main() {
     }
 }
 
+fn filename_to_vec_string(filename: &str) -> Vec<String> {
+    let f = File::open(filename).expect("could open file");
+    let file = BufReader::new(&f);
+    file.lines()
+        .map(|line| {
+            let l = line.expect("Could not parse line");
+            l
+        })
+        .collect()
+}
+
+fn language_to_vec_string(language: &str) -> Vec<String> {
+    language.split("\n").map(|l| l.to_string()).collect()
+}
+
 #[allow(unused_variables)]
 fn run() -> Result<(), Error> {
+    let filename = "songs/test/template.socool";
+    let mut language = "".to_string();
+    let f = File::open(filename)?;
+    let file = BufReader::new(&f);
+    file.lines().for_each(|line| {
+        let l = line.expect("Could not parse line");
+        language.push_str(&l);
+        language.push_str("\n");
+    });
+
+    let from_filename = filename_to_vec_string(filename);
+    let from_language = language_to_vec_string(language.as_str());
+
+    for (a, b) in from_filename.iter().zip(&from_language) {
+        assert_eq!(a, b);
+    }
+
     Ok(())
 }
