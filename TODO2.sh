b7fcb3ee4817bba33e3a013de0f4e6b48099e309
Danny Meyer <Danny.Meyer@gmail.com>
2020-02-12 18:53:39 -0500

    WIP

---
+++ TODO2.sh
@@ -0,0 +1,69 @@
+[x] OpOrNf => Term
+[x] OpOrNfTable => TermTable
+[x] Op::FunctionDef => Term::FunDef
+[] Op::Fid => Op::Id
+
+Op =
+  Seq of ListOp
+| Overlay of List<Op>
+| Compose of Op, Op
+| ...
+
+list melody (scale) = scale @ [ 1, 2, 3, 4, 5, 4, 5]
+
+melody (ET12)
+
+melody (ET17)
+
+pub enum Term {
+    Id(String),
+    Op(Op),
+    Nf(NormalForm),
+    FunDef(FunDef),
+}
+
+p10.1.2.93ub struct FunDef {
+    pub name: String,
+    pub vars: Vec<String>,
+    pub term: Box<Term>,
+}
+
+fn1(a, b) = {
+  Seq [a, b, a]
+}
+
+fn2(c, d) {
+  Seq [c, d, c]
+
+main = {
+  fn2(fn1(Tm 1, Tm 3/2), fn1(Tm 9/8, Tm 4/3))
+}
+
+pub trait Substitute {
+    fn substitute(&self, normal_form: &mut NormalForm, table: &TermTable, arg_map: &ArgMap)
+        -> Term;
+}
+
+Term ->
+Nf 
+  - Cool!
+Op 
+  - Normalize it.
+  - Store it. 
+FunDef
+  - (Simplify it)
+  - Store it. 
+
+Id(name)
+  - Look it up
+  - Replace it
+
+Term =
+| Op(op)
+| Nf(nf)
+| FunDef(FunDef)
+#| ListDef(Lop)
+#| ListDef(List(Nf))
+
+
+     
