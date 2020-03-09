d19b154be60ef86d13223e225873ed1d1656ab1a
Danny Meyer <Danny.Meyer@gmail.com>
2020-03-09 18:29:32 -0400

    WIP

--- ast/src/lists/substitute_list.rs
+++ ast/src/lists/substitute_list.rs
@@ -4,34 +4,29 @@ use crate::{Defs, ListOp, Term};
 
 impl Substitute for ListOp {
     fn substitute(&self, normal_form: &mut NormalForm, defs: &Defs, arg_map: &ArgMap) -> Term {
-        unimplemented!()
-        //match self {
-        //ListOp::IndexedNamedList { name, indices } => {
-        //let value = arg_map.get(&name.clone());
-        //let term = match value {
-        //Some(sub) => sub.clone(),
-        //None => handle_id_error(name.to_string(), defs),
-        //};
-        //match term {
-        //Term::Lop(list_op) => match list_op {
-        //ListOp::List(list) => {
-        //let new_lop = ListOp::IndexedList {
-        //terms: list,
-        //indices: indices.clone(),
-        //};
-        //new_lop.substitute(normal_form, defs, arg_map)
-        //}
-        //_ => unimplemented!(),
-        //},
-        //_ => unimplemented!(),
-        //}
-        //}
-        //_ => {
-        //let vec_nf = self.to_list_nf(normal_form, defs);
-        //let vec_terms = vec_nf.iter().map(|t| Term::Nf(t.clone())).collect();
-        //let terms = substitute_operations(vec_terms, normal_form, defs, arg_map);
-        //Term::Lop(ListOp::List(terms))
-        //}
-        //}
+        match self {
+            ListOp::Const(terms) => Term::Lop(ListOp::Const(substitute_operations(
+                terms.to_vec(),
+                normal_form,
+                defs,
+                arg_map,
+            ))),
+            ListOp::Named(name) => {
+                let arg_term = arg_map.get(name);
+                let term = match arg_term {
+                    Some(arg_term) => arg_term.clone(),
+                    None => handle_id_error(name.to_string(), defs),
+                };
+
+                match term {
+                    Term::Lop(lop) => lop.substitute(normal_form, defs, arg_map),
+                    _ => unimplemented!(),
+                }
+            }
+            ListOp::ListOpIndexed {
+                list_op: _,
+                indices: _,
+            } => unimplemented!(),
+        }
     }
 }
