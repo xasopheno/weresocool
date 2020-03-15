1749070e71d4163d43121e7ffcaadddf54475934
Danny Meyer <Danny.Meyer@gmail.com>
2020-03-15 01:10:59 -0400

    WIP

--- bin/pad.rs
+++ bin/pad.rs
@@ -1,4 +1,7 @@
 use failure::Fail;
+use std::fs::{self};
+use std::io;
+use std::path::Path;
 use weresocool_error::Error;
 
 fn main() {
@@ -14,5 +17,22 @@ fn main() {
 
 #[allow(unused_variables)]
 fn run() -> Result<(), Error> {
+    visit_dirs(Path::new("./mocks"));
+
+    Ok(())
+}
+
+// one possible implementation of walking a directory only visiting files
+fn visit_dirs(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            //dbg!(&path.is_dir());
+            if path.is_dir() {
+                dbg!(path);
+            }
+        }
+    }
     Ok(())
 }
