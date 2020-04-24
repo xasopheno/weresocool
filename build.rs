fad674191955cc9b531248cc3fab6b56a6cb4634
Danny Meyer <Danny.Meyer@gmail.com>
2020-04-24 13:51:53 -0400

    cleanup

--- build.rs
+++ build.rs
@@ -1,10 +1,9 @@
 fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script.
+    // For building portaudio statically
     println!("cargo:rustc-link-lib=static=portaudio");
     println!("cargo:rustc-link-lib=framework=CoreAudio");
     println!("cargo:rustc-link-lib=framework=AudioUnit");
     println!("cargo:rustc-link-lib=framework=Carbon");
     println!("cargo:rustc-link-lib=framework=AudioToolbox");
-    //println!("cargo:rustc-link-search=[static=]PATH");
 }
 
