edf2da9878ee11dcb517f4e6d30f82efa7b01a2c
Danny <danny.meyer@gmail.com>
2020-06-06 02:41:15 -0400

    Build PortAudio statically in weresocool_server (#262)
    * static build
    
    * WIP
    
    * adds portaudio to application
    
    * images already have brew

---
+++ application/server/build_static.sh
@@ -0,0 +1,2 @@
+PORTAUDIO_ONLY_STATIC=true RUSTFLAGS='-L /usr/local/opt/portaudio/lib/' cargo build --release
+
--- build_static.rs
+++
@@ -1,4 +0,0 @@
-PORTAUDIO_ONLY_STATIC=true RUSTFLAGS='-L /usr/local/opt/portaudio/lib/' cargo build --release
-
-
-
