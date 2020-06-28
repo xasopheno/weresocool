80bc948ad61eeb4c2a42f699f04e1aa75d32ea41
Danny Meyer <Danny.Meyer@gmail.com>
2020-06-27 21:12:08 -0400

    move buttons

--- application/app/components/Ratios.tsx
+++ application/app/components/Ratios.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import path from 'path';
-import { remote } from 'electron';
-import { Demo, DemoData } from './Tutorial';
-import { tutorial_list, album_list } from './tutorial_list';
+// import path from 'path';
+// import { remote } from 'electron';
+// import { Demo, DemoData } from './Tutorial';
+// import { tutorial_list, album_list } from './tutorial_list';
 import { RatioChart } from './RatioChart';
 
 const RSpace = styled.div`
   position: absolute;
-  top: 10%;
+  top: 10;
   right: 0;
   display: flex;
   flex-direction: column;
@@ -19,98 +19,69 @@ const RSpace = styled.div`
   border: 5px ridge goldenrod;
 `;
 
-const MagicButton = styled.img`
-  width: 70px;
-  height: 70px;
-  border-top: 5px ridge goldenrod;
-  opacity: 0.7;
-  background-color: red;
-  :hover {
-    opacity: 1;
-  }
-`;
+// const MagicButton = styled.img`
+// width: 70px;
+// height: 70px;
+// border-top: 5px ridge goldenrod;
+// opacity: 0.7;
+// background-color: red;
+// :hover {
+// opacity: 1;
+// }
+// `;
 
-const MagicButtonSmall = styled.img`
-  width: 40px;
-  height: 40px;
-  border-top: 5px ridge goldenrod;
-  opacity: 0.7;
-  background-color: red;
-  :hover {
-    opacity: 1;
-  }
-`;
+// const MagicButtonSmall = styled.img`
+// width: 40px;
+// height: 40px;
+// border-top: 5px ridge goldenrod;
+// opacity: 0.7;
+// background-color: red;
+// :hover {
+// opacity: 1;
+// }
+// `;
 
 export const Ratios = (props: { width: number }): React.ReactElement | null => {
-  const assetsPath = remote.app.isPackaged
-    ? path.join(process.resourcesPath, 'extraResources/assets')
-    : '../extraResources/assets';
-  const [showTutorial, setShowTutorial] = React.useState(false);
-  const [showDemo, setShowDemo] = React.useState(false);
+  // const assetsPath = remote.app.isPackaged
+  // ? path.join(process.resourcesPath, 'extraResources/assets')
+  // : '../extraResources/assets';
+  // const [showTutorial, setShowTutorial] = React.useState(false);
+  // const [showDemo, setShowDemo] = React.useState(false);
 
-  const showTutorialModal = (b: boolean) => {
-    setShowTutorial(b);
-  };
+  // const showTutorialModal = (b: boolean) => {
+  // setShowTutorial(b);
+  // };
 
-  const showDemoModal = (b: boolean) => {
-    setShowDemo(b);
-  };
+  // const showDemoModal = (b: boolean) => {
+  // setShowDemo(b);
+  // };
 
-  const tutorialData: DemoData = {
-    title: 'Cool Tutorials',
-    setShow: showTutorialModal,
-    show: showTutorial,
-    data: tutorial_list,
-    folder: 'tutorial',
-  };
+  // const tutorialData: DemoData = {
+  // title: 'Cool Tutorials',
+  // setShow: showTutorialModal,
+  // show: showTutorial,
+  // data: tutorial_list,
+  // folder: 'tutorial',
+  // };
 
-  const demoData: DemoData = {
-    title: 'Cool Demos',
-    setShow: showDemoModal,
-    show: showDemo,
-    data: album_list,
-    folder: 'demo',
-  };
+  // const demoData: DemoData = {
+  // title: 'Cool Demos',
+  // setShow: showDemoModal,
+  // show: showDemo,
+  // data: album_list,
+  // folder: 'demo',
+  // };
 
   if (props.width > 1000) {
     return (
       <div>
-        <Demo demoData={tutorialData} />
-        <Demo demoData={demoData} />
         <RSpace id="ratios">
           <RatioChart />
-          <MagicButton
-            id={'magicButton'}
-            src={`${assetsPath}/magic.png`}
-            onClick={() => showDemoModal(true)}
-          />
-          <MagicButton
-            id={'questionButton'}
-            src={`${assetsPath}/question_mark.jpg`}
-            onClick={() => showTutorialModal(true)}
-          />
         </RSpace>
       </div>
     );
   } else if (props.width > 700) {
-    return (
-      <div>
-        <Demo demoData={tutorialData} />
-        <Demo demoData={demoData} />
-        <RSpace id="ratios">
-          <MagicButtonSmall
-            id={'magicButton'}
-            src={`${assetsPath}/magic.png`}
-            onClick={() => showDemoModal(true)}
-          />
-          <MagicButtonSmall
-            id={'questionButton'}
-            src={`${assetsPath}/question_mark.jpg`}
-            onClick={() => showTutorialModal(true)}
-          />
-        </RSpace>
-      </div>
-    );
+    return <div />;
   } else {
     return <div />;
   }
