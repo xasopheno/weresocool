d830e9ec7a019ae38763bd3d6cf60e839b3632d0
Danny Meyer <Danny.Meyer@gmail.com>
2020-07-21 00:26:45 -0400

    editor scaling

--- application/app/components/Ratios.tsx
+++ application/app/components/Ratios.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-// import path from 'path';
-// import { remote } from 'electron';
-// import { Demo, DemoData } from './Tutorial';
-// import { tutorial_list, album_list } from './tutorial_list';
 import { RatioChart } from './RatioChart';
 
 const RSpace = styled.div`
@@ -19,59 +15,7 @@ const RSpace = styled.div`
   border: 5px ridge goldenrod;
 `;
 
-// const MagicButton = styled.img`
-// width: 70px;
-// height: 70px;
-// border-top: 5px ridge goldenrod;
-// opacity: 0.7;
-// background-color: red;
-// :hover {
-// opacity: 1;
-// }
-// `;
-
-// const MagicButtonSmall = styled.img`
-// width: 40px;
-// height: 40px;
-// border-top: 5px ridge goldenrod;
-// opacity: 0.7;
-// background-color: red;
-// :hover {
-// opacity: 1;
-// }
-// `;
-
 export const Ratios = (props: { width: number }): React.ReactElement | null => {
-  // const assetsPath = remote.app.isPackaged
-  // ? path.join(process.resourcesPath, 'extraResources/assets')
-  // : '../extraResources/assets';
-  // const [showTutorial, setShowTutorial] = React.useState(false);
-  // const [showDemo, setShowDemo] = React.useState(false);
-
-  // const showTutorialModal = (b: boolean) => {
-  // setShowTutorial(b);
-  // };
-
-  // const showDemoModal = (b: boolean) => {
-  // setShowDemo(b);
-  // };
-
-  // const tutorialData: DemoData = {
-  // title: 'Cool Tutorials',
-  // setShow: showTutorialModal,
-  // show: showTutorial,
-  // data: tutorial_list,
-  // folder: 'tutorial',
-  // };
-
-  // const demoData: DemoData = {
-  // title: 'Cool Demos',
-  // setShow: showDemoModal,
-  // show: showDemo,
-  // data: album_list,
-  // folder: 'demo',
-  // };
-
   if (props.width > 1000) {
     return (
       <div>
