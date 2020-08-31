1876a65149bb88d04973b1971a2bd3c70ddb1397
xasopheno <danny@xasopheno.com>
2020-08-30 22:15:30 -0400

    some cleanup

--- bin/pad.rs
+++ bin/pad.rs
@@ -1,7 +1,7 @@
-use walkdir::WalkDir;
 // use num_rational::Rational64;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
+use walkdir::WalkDir;
 use weresocool::generation::{RenderReturn, RenderType};
 use weresocool::interpretable::{InputType::Filename, Interpretable};
 use weresocool_ast::{NormalForm, OscType, PointOp};
@@ -115,10 +115,6 @@ fn test_normalize() {
     assert_eq!(result, expected);
 }
 
-// let op = PointOp::init();
-// let data_op = DataOp::from_point_op(op);
-// dbg!(data_op);
-//
 fn main() -> Result<(), Error> {
     let (min_state, max_state) = find_min_max_from_dir()?;
     let normalizer = Normalizer::from_min_max(min_state, max_state);
@@ -131,24 +127,33 @@ fn main() -> Result<(), Error> {
     };
 
     let normalized: Vec<Vec<DataOp>> = nf_to_normalized_vec_data_op(&nf, &normalizer);
-    dbg!(&normalized);
+    // dbg!(&normalized);
 
     let mut file = OpenOptions::new()
-        .truncate(true)
         .create(true)
         .write(true)
-        .append(true)
+        .truncate(true)
         .open("data.csv")
         .unwrap();
 
-    for op in normalized.iter().flatten() {
-        dbg!(op);
-        file.write(format!("{}\n", op.fm.to_string()).as_bytes())?;
+    for voice in normalized {
+        for op in voice {
+            // dbg!(op);
+            file.write(
+                format!(
+                    "{}, {}, {}, {}, {}, {}, {}\n",
+                    op.fm.to_string(),
+                    op.fa.to_string(),
+                    op.pm.to_string(),
+                    op.pa.to_string(),
+                    op.l.to_string(),
+                    op.g.to_string(),
+                    op.osc_type.to_string()
+                )
+                .as_bytes(),
+            )?;
+        }
     }
-
-    // if let Err(e) = writeln!(file, "A new line!") {
-    // eprintln!("Couldn't write to file: {}", e);
-    // }
 
     Ok(())
 }
@@ -158,6 +163,42 @@ fn main() -> Result<(), Error> {
 // [[1, 2, 3, 2, 1], [3, 4, 2, 4, 3]],
 // [[1, 2, 3, 2, 1], [3, 4, 2, 4, 3]],
 // ]
+//
+fn get_file_names() -> Vec<String> {
+    let demo_dir = "./application/extraResources/demo/";
+    let mut result = vec![];
+
+    let paths = WalkDir::new(demo_dir)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .collect::<Vec<walkdir::DirEntry>>();
+
+    for entry in paths {
+        let f_name = entry.path().to_string_lossy().to_string();
+        if f_name.ends_with(".socool")
+            && ![
+                "dunno.socool",
+                "tokyo.socool",
+                "wonder_ball.socool",
+                "television.socool",
+                "songsongsong.socool",
+                "modby.socool",
+                "hilbert.socool",
+                "how_to_build.socool",
+                "how_to_fight.socool",
+                "dunno2.socool",
+                "for_two_ears.socool",
+            ]
+            .iter()
+            .any(|&name| demo_dir.clone().to_owned() + name == f_name)
+        {
+            result.push(f_name.to_string());
+        }
+    }
+
+    result
+}
 
 fn find_min_max_from_dir() -> Result<(DataOp, DataOp), Error> {
     let mut max_state = DataOp {
@@ -179,55 +220,67 @@ fn find_min_max_from_dir() -> Result<(DataOp, DataOp), Error> {
         osc_type: 0.0,
     };
 
-    for entry in WalkDir::new("./application/extraResources/demo/")
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
-        let f_name = entry.path().to_string_lossy();
-        if f_name.ends_with(".socool") {
-            println!("{:?}", f_name);
-            let render_return = Filename(&f_name).make(RenderType::NfBasisAndTable, None)?;
-            let (nf, _, _) = match render_return {
-                RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
-                _ => panic!("huh"),
-            };
+    let mut max_ops = 0;
+    let mut max_voices = 0;
+    let mut n_ops: Vec<usize> = vec![];
+    let mut n_voices: Vec<(String, usize)> = vec![];
 
-            let _data_ops: Vec<Vec<DataOp>> = nf
-                .operations
-                .iter()
-                .map(|voice| {
-                    voice
-                        .iter()
-                        .map(|op| {
-                            let data_op = DataOp::from_point_op(op.to_owned());
-                            max_state = DataOp {
-                                fm: max_state.fm.max(data_op.fm),
-                                fa: max_state.fa.max(data_op.fa),
-                                pm: max_state.pm.max(data_op.pm),
-                                pa: max_state.pa.max(data_op.pa),
-                                g: max_state.g.max(data_op.g),
-                                l: max_state.l.max(data_op.l),
-                                ..max_state
-                            };
-                            min_state = DataOp {
-                                fm: min_state.fm.min(data_op.fm),
-                                fa: min_state.fa.min(data_op.fa),
-                                pm: min_state.pm.min(data_op.pm),
-                                pa: min_state.pa.min(data_op.pa),
-                                g: min_state.g.min(data_op.g),
-                                l: min_state.l.min(data_op.l),
-                                ..min_state
-                            };
+    for f_name in get_file_names() {
+        // println!("{:?}", f_name);
+        let render_return = Filename(&f_name).make(RenderType::NfBasisAndTable, None)?;
+        let (nf, _, _) = match render_return {
+            RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
+            _ => panic!("huh"),
+        };
 
-                            data_op
-                        })
-                        .collect()
-                })
-                .collect();
-        }
+        max_voices = usize::max(max_voices, nf.operations.len());
+        n_voices.push((f_name.to_string(), nf.operations.len()));
+
+        let _data_ops: Vec<Vec<DataOp>> = nf
+            .operations
+            .iter()
+            .map(|voice| {
+                max_ops = usize::max(max_ops, voice.len());
+                n_ops.push(voice.len());
+                // if voice.len() > 3_000 {
+                // dbg!("long_voice", &f_name);
+                // };
+                voice
+                    .iter()
+                    .map(|op| {
+                        let data_op = DataOp::from_point_op(op.to_owned());
+                        max_state = DataOp {
+                            fm: max_state.fm.max(data_op.fm),
+                            fa: max_state.fa.max(data_op.fa),
+                            pm: max_state.pm.max(data_op.pm),
+                            pa: max_state.pa.max(data_op.pa),
+                            g: max_state.g.max(data_op.g),
+                            l: max_state.l.max(data_op.l),
+                            ..max_state
+                        };
+                        min_state = DataOp {
+                            fm: min_state.fm.min(data_op.fm),
+                            fa: min_state.fa.min(data_op.fa),
+                            pm: min_state.pm.min(data_op.pm),
+                            pa: min_state.pa.min(data_op.pa),
+                            g: min_state.g.min(data_op.g),
+                            l: min_state.l.min(data_op.l),
+                            ..min_state
+                        };
+
+                        data_op
+                    })
+                    .collect()
+            })
+            .collect();
     }
+
     println!("MAX {:#?}\nMIN {:#?}\n", max_state, min_state);
+    // println!("N VOICES {:#?}\n", n_voices);
+    println!("N_Compositions {:#?}\n", n_voices.len());
+    println!("Min NOPS {:?}\n", n_ops.iter().min());
+    println!("Max NOPS {:?}\n", n_ops.iter().max());
+    println!("Max_voices {:?}\n", max_voices);
     Ok((min_state, max_state))
 }
 
