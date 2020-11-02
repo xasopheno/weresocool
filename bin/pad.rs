a69996d64cbec9ca3e4825cdfbcc38c601cded78
xasopheno <danny@xasopheno.com>
2020-11-02 17:13:55 -0500

    wip

--- bin/pad.rs
+++ bin/pad.rs
@@ -2,87 +2,63 @@ extern crate anyhow;
 extern crate cpal;
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{BufferSize, StreamConfig};
 
 fn main() -> Result<(), anyhow::Error> {
-    // Conditionally compile with jack if the feature is specified.
-    #[cfg(all(
-        any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd"),
-        feature = "jack"
-    ))]
-    // Manually check for flags. Can be passed through cargo with -- e.g.
-    // cargo run --release --example beep --features jack -- --jack
-    let host = if std::env::args()
-        .collect::<String>()
-        .contains(&String::from("--jack"))
-    {
-        cpal::host_from_id(cpal::available_hosts()
-            .into_iter()
-            .find(|id| *id == cpal::HostId::Jack)
-            .expect(
-                "make sure --features jack is specified. only works on OSes where jack is available",
-            )).expect("jack host unavailable")
-    } else {
-        cpal::default_host()
-    };
+  let host = cpal::default_host();
 
-    #[cfg(any(
-        not(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd")),
-        not(feature = "jack")
-    ))]
-    let host = cpal::default_host();
+  let device = host
+    .default_output_device()
+    .expect("failed to find a default output device");
+  // let config = device.default_output_config()?;
+  // dbg!(config.buffer_size());
+  //
+  let config = StreamConfig {
+    channels: 2,
+    buffer_size: BufferSize::Fixed(1024 * 2),
+    sample_rate: cpal::SampleRate(44_100),
+    // buffer_size: BufferSize::Default,
+  };
+  // config.buffer_size;
 
-    let device = host
-        .default_output_device()
-        .expect("failed to find a default output device");
-    let config = device.default_output_config()?;
+  run(&device, &config.into())?;
 
-    match config.sample_format() {
-        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into())?,
-        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into())?,
-        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into())?,
-    }
-
-    Ok(())
+  Ok(())
 }
 
-fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
-where
-    T: cpal::Sample,
-{
-    let sample_rate = config.sample_rate.0 as f32;
-    let channels = config.channels as usize;
+fn run(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error> {
+  let sample_rate = config.sample_rate.0 as f32;
+  let channels = config.channels as usize;
 
-    // Produce a sinusoid of maximum amplitude.
-    let mut sample_clock = 0f32;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
-    };
+  // Produce a sinusoid of maximum amplitude.
+  let mut sample_clock = 0f32;
+  let mut next_value = move || {
+    sample_clock = (sample_clock + 1.0) % sample_rate;
+    (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin() * 0.008
+  };
 
-    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+  let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
 
-    let stream = device.build_output_stream(
-        config,
-        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-            write_data(data, channels, &mut next_value)
-        },
-        err_fn,
-    )?;
-    stream.play()?;
+  let stream = device.build_output_stream(
+    config,
+    move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+      write_data(data, channels, &mut next_value)
+    },
+    err_fn,
+  )?;
+  stream.play()?;
 
-    std::thread::sleep(std::time::Duration::from_millis(1000));
+  std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    Ok(())
+  Ok(())
 }
 
-fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
-where
-    T: cpal::Sample,
-{
-    for frame in output.chunks_mut(channels) {
-        let value: T = cpal::Sample::from::<f32>(&next_sample());
-        for sample in frame.iter_mut() {
-            *sample = value;
-        }
+fn write_data(output: &mut [f32], channels: usize, next_sample: &mut dyn FnMut() -> f32) {
+  dbg!(&output.len());
+  for frame in output.chunks_mut(channels) {
+    let value: f32 = cpal::Sample::from(&next_sample());
+    for sample in frame.iter_mut() {
+      *sample = value;
     }
+  }
 }
