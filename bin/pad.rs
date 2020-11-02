a5a00617d3ef8a42706c02a372eabeabada5493f
Danny Meyer <danny@xasopheno.com>
2020-11-02 00:31:06 -0500

    wip

--- bin/pad.rs
+++ bin/pad.rs
@@ -1,91 +1,88 @@
-//#![allow(dead_code, unused_imports, unused_variables)]
-use failure::Fail;
-use std::sync::mpsc::channel;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
-use weresocool::{
-    generation::parsed_to_render::{RenderReturn, RenderType},
-    interpretable::{InputType, Interpretable},
-    manager::RenderManager,
-    portaudio::real_time_render_manager,
-    ui::were_so_cool_logo,
-};
-use weresocool_error::Error;
-use weresocool_instrument::renderable::{
-    nf_to_vec_renderable, renderables_to_render_voices, RenderVoice,
-};
+extern crate anyhow;
+extern crate cpal;
 
-fn main() {
-    match run() {
-        Ok(_) => {}
-        e => {
-            for cause in Fail::iter_causes(&e.unwrap_err()) {
-                println!("Failure caused by: {}", cause);
-            }
-        }
-    }
-}
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
-pub fn prepare_render(input: InputType<'_>) -> Result<Vec<RenderVoice>, Error> {
-    let (nf, basis, table) = match input.make(RenderType::NfBasisAndTable, None)? {
-        RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
-        _ => panic!("Error. Unable to generate NormalForm"),
+fn main() -> Result<(), anyhow::Error> {
+    // Conditionally compile with jack if the feature is specified.
+    #[cfg(all(
+        any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd"),
+        feature = "jack"
+    ))]
+    // Manually check for flags. Can be passed through cargo with -- e.g.
+    // cargo run --release --example beep --features jack -- --jack
+    let host = if std::env::args()
+        .collect::<String>()
+        .contains(&String::from("--jack"))
+    {
+        cpal::host_from_id(cpal::available_hosts()
+            .into_iter()
+            .find(|id| *id == cpal::HostId::Jack)
+            .expect(
+                "make sure --features jack is specified. only works on OSes where jack is available",
+            )).expect("jack host unavailable")
+    } else {
+        cpal::default_host()
     };
-    let renderables = nf_to_vec_renderable(&nf, &table, &basis)?;
 
-    let render_voices = renderables_to_render_voices(renderables);
+    #[cfg(any(
+        not(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd")),
+        not(feature = "jack")
+    ))]
+    let host = cpal::default_host();
 
-    Ok(render_voices)
-}
+    let device = host
+        .default_output_device()
+        .expect("failed to find a default output device");
+    let config = device.default_output_config()?;
 
-fn run() -> Result<(), Error> {
-    were_so_cool_logo();
-    println!("       )))***=== REAL<COOL>TIME *buffered ===***(((  \n ");
-
-    let filename1 = "songs/dance/skip.socool";
-    let filename2 = "songs/dance/candle.socool";
-
-    let render_manager = Arc::new(Mutex::new(RenderManager::init_silent()));
-    let render_manager_clone = render_manager.clone();
-
-    let (send, recv) = channel();
-    println!("Start...");
-    thread::Builder::new()
-        .name("Sender".to_string())
-        .spawn(move || {
-            thread::sleep(Duration::from_secs(1));
-            for _ in 0..4 {
-                send.send(InputType::Filename(&filename1)).unwrap();
-                thread::sleep(Duration::from_secs(4));
-                send.send(InputType::Filename(&filename2)).unwrap();
-                thread::sleep(Duration::from_secs(4));
-            }
-        })?;
-
-    thread::Builder::new()
-        .name("Receiver".to_string())
-        .spawn(move || loop {
-            if let Ok(v) = recv.try_recv() {
-                println!("language received");
-                let render = prepare_render(v);
-
-                match render {
-                    Ok(r) => {
-                        render_manager_clone.lock().unwrap().push_render(r);
-                        println!("Render Success")
-                    }
-                    _ => panic!("Render Failure"),
-                }
-            };
-        })?;
-
-    let mut stream = real_time_render_manager(Arc::clone(&render_manager))?;
-    stream.start()?;
-
-    while let true = stream.is_active()? {}
-
-    stream.stop()?;
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into())?,
+        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into())?,
+        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into())?,
+    }
 
     Ok(())
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
+where
+    T: cpal::Sample,
+{
+    let sample_rate = config.sample_rate.0 as f32;
+    let channels = config.channels as usize;
+
+    // Produce a sinusoid of maximum amplitude.
+    let mut sample_clock = 0f32;
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
+    };
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+    )?;
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+
+    Ok(())
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: cpal::Sample,
+{
+    for frame in output.chunks_mut(channels) {
+        let value: T = cpal::Sample::from::<f32>(&next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
 }
