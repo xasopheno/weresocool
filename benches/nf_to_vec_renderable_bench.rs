f233db586070e684a3718fbd7343475e9dce7ff6
xasopheno <danny@xasopheno.com>
2020-12-14 15:12:32 -0500

    improve benches

--- benches/nf_to_vec_renderable_bench.rs
+++ benches/nf_to_vec_renderable_bench.rs
@@ -9,18 +9,17 @@ use weresocool_instrument::renderable::nf_to_vec_renderable;
 
 fn nf_to_vec_renderable_bench(c: &mut Criterion) {
     let filename = "songs/test/render_op_get_batch.socool".to_string();
-    let (nf, basis, table) = match Filename(&filename)
-        .make(RenderType::NfBasisAndTable, None)
-        .unwrap()
-    {
-        RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
-        _ => {
-            panic!();
-        }
-    };
-
     c.bench_function("nf_to_vec_renderable_bench", |b| {
         b.iter(|| {
+            let (nf, basis, table) = match Filename(&filename)
+                .make(RenderType::NfBasisAndTable, None)
+                .unwrap()
+            {
+                RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
+                _ => {
+                    panic!();
+                }
+            };
             let _renderables = nf_to_vec_renderable(&nf, &table, &basis);
         })
     });
