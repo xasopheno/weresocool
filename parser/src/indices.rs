7c49def92b334b9d665768743ddee383b7cbfcd1
xasopheno <danny@xasopheno.com>
2020-12-14 11:48:12 -0500

    simpler ets

--- parser/src/indices.rs
+++ parser/src/indices.rs
@@ -11,7 +11,7 @@ pub fn et(d: i64) -> Vec<Term> {
     for i in 1..d as usize {
         let m = 2.0_f32.powf(i as f32 / d as f32);
         ops.push(Term::Op(Op::TransposeM {
-            m: f32_to_rational(m.to_string()),
+            m: f32_to_rational(format!("{:.4}", m)),
         }))
     }
 
