d7646276d011a8c4974de8725fdefef687683bf5
Danny <danny.meyer@gmail.com>
2020-12-14 15:14:05 -0500

    improve benches (#536)
    Co-authored-by: xasopheno <danny@xasopheno.com>

--- benches/render_batch_bench.rs
+++ benches/render_batch_bench.rs
@@ -11,23 +11,27 @@ use weresocool_instrument::{
 
 fn render_batch_bench(c: &mut Criterion) {
     let filename = "songs/test/render_op_get_batch.socool".to_string();
-    let (nf, basis, table) = match Filename(&filename)
-        .make(RenderType::NfBasisAndTable, None)
-        .unwrap()
-    {
-        RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
-        _ => {
-            panic!();
-        }
-    };
-
-    let renderables = nf_to_vec_renderable(&nf, &table, &basis).unwrap();
-    let mut voices1 = renderables_to_render_voices(renderables);
     c.bench_function("render_batch", |b| {
         b.iter(|| {
+            let (nf, basis, table) = match Filename(&filename)
+                .make(RenderType::NfBasisAndTable, None)
+                .unwrap()
+            {
+                RenderReturn::NfBasisAndTable(nf, basis, table) => (nf, basis, table),
+                _ => {
+                    panic!();
+                }
+            };
+
+            let renderables = nf_to_vec_renderable(&nf, &table, &basis).unwrap();
+            let mut voices1 = renderables_to_render_voices(renderables);
             let _r: Vec<StereoWaveform> = voices1
                 .iter_mut()
-                .map(|voice| voice.render_batch(black_box(1024), None).unwrap())
+                .map(|voice| {
+                    voice
+                        .render_batch(1024, None)
+                        .unwrap_or_else(|| StereoWaveform::new_empty())
+                })
                 .collect();
         })
     });
