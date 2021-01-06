b6b619effda4582f4a88b348f59929d382d8fee0
xasopheno <danny@xasopheno.com>
2021-01-06 18:15:25 -0500

    add mocks to watched

--- build.rs
+++ build.rs
@@ -2,8 +2,9 @@ use std::env;
 
 fn main() {
     let static_build = matches!(env::var("PORTAUDIO_ONLY_STATIC"), Ok(_e));
-
     println!("cargo:rerun-if-env-changed=PORTAUDIO_ONLY_STATIC");
+
+    println!("cargo:rerun-if-changed=./mocks/");
     if cfg!(target_os = "macos") && static_build {
         println!("cargo:rustc-link-lib=static=portaudio");
         println!("cargo:rustc-link-lib=framework=CoreAudio");
